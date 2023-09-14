f8da470067418a885643b5fcbb123687f860f6a1
md4096 <144923189+md4096@users.noreply.github.com>
2023-09-14 14:51:18 +0200

    Upgrade to Material 3

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -29,7 +29,7 @@ kotlin {
             dependencies {
                 implementation(compose.runtime)
                 implementation(compose.foundation)
-                implementation(compose.material)
+                implementation(compose.material3)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
                 api("at.asitplus.wallet:vclib:3.0.1")
