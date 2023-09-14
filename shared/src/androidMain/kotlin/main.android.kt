819d00a8570da57c244add7633105496b03710b4
Christian Kollmann <christian.kollmann@a-sit.at>
2023-09-14 13:07:25 +0200

    Fix Android compilation

--- shared/src/androidMain/kotlin/main.android.kt
+++ shared/src/androidMain/kotlin/main.android.kt
@@ -1,7 +1,14 @@
 import androidx.compose.runtime.Composable
+import at.asitplus.KmmResult
+import at.asitplus.wallet.app.common.ObjectFactory
 import at.asitplus.wallet.app.common.WalletMain
+import at.asitplus.wallet.lib.agent.CryptoService
 import at.asitplus.wallet.lib.agent.DefaultCryptoService
 
 actual fun getPlatformName(): String = "Android"
 
-@Composable fun MainView() = App(WalletMain(DefaultCryptoService()))
+@Composable fun MainView() = App(WalletMain(objectFactory = object : ObjectFactory{
+    override suspend fun loadCryptoService(): KmmResult<CryptoService> {
+        return KmmResult.success(DefaultCryptoService())
+    }
+}))
