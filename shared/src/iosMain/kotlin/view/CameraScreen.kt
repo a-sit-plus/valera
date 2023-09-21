d826a7f68574e1301fbf939c2db4bd15ec970b84
md4096 <144923189+md4096@users.noreply.github.com>
2023-09-21 14:14:01 +0200

    Add iOS Scanner

--- shared/src/iosMain/kotlin/view/CameraScreen.kt
+++ shared/src/iosMain/kotlin/view/CameraScreen.kt
@@ -1,5 +1,285 @@
 package view
 
-actual fun CameraView(onFoundPayload: (text: String) -> Unit){
+import globalBack
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import platform.AVFoundation.AVAuthorizationStatusAuthorized
+import platform.AVFoundation.AVAuthorizationStatusDenied
+import platform.AVFoundation.AVAuthorizationStatusNotDetermined
+import platform.AVFoundation.AVAuthorizationStatusRestricted
+import platform.AVFoundation.AVCaptureDevice
+import platform.AVFoundation.AVMediaTypeVideo
+import platform.AVFoundation.authorizationStatusForMediaType
+import platform.AVFoundation.requestAccessForMediaType
+import androidx.compose.material3.Text
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.interop.UIKitView
+import androidx.compose.ui.unit.dp
+import kotlinx.cinterop.CValue
+import kotlinx.cinterop.ObjCAction
+import platform.AVFoundation.AVCaptureDeviceDiscoverySession
+import platform.AVFoundation.AVCaptureDeviceInput
+import platform.AVFoundation.AVCaptureDevicePositionBack
+import platform.AVFoundation.AVCaptureDeviceTypeBuiltInDualCamera
+import platform.AVFoundation.AVCaptureDeviceTypeBuiltInDualWideCamera
+import platform.AVFoundation.AVCaptureDeviceTypeBuiltInDuoCamera
+import platform.AVFoundation.AVCaptureDeviceTypeBuiltInUltraWideCamera
+import platform.AVFoundation.AVCaptureDeviceTypeBuiltInWideAngleCamera
+import platform.AVFoundation.AVCaptureMetadataOutput
+import platform.AVFoundation.AVCaptureMetadataOutputObjectsDelegateProtocol
+import platform.AVFoundation.AVCapturePhotoOutput
+import platform.AVFoundation.AVCaptureSession
+import platform.AVFoundation.AVCaptureSessionPresetPhoto
+import platform.AVFoundation.AVCaptureVideoOrientationLandscapeLeft
+import platform.AVFoundation.AVCaptureVideoOrientationLandscapeRight
+import platform.AVFoundation.AVCaptureVideoOrientationPortrait
+import platform.AVFoundation.AVCaptureVideoPreviewLayer
+import platform.AVFoundation.AVLayerVideoGravityResizeAspectFill
+import platform.AVFoundation.AVMetadataMachineReadableCodeObject
+import platform.AVFoundation.AVMetadataObjectTypeQRCode
+import platform.CoreGraphics.CGRect
+import platform.Foundation.NSNotification
+import platform.Foundation.NSNotificationCenter
+import platform.Foundation.NSSelectorFromString
+import platform.QuartzCore.CATransaction
+import platform.QuartzCore.kCATransactionDisableActions
+import platform.UIKit.UIDevice
+import platform.UIKit.UIDeviceOrientation
+import platform.UIKit.UIDeviceOrientationDidChangeNotification
+import platform.UIKit.UIView
+import platform.darwin.NSObject
+import platform.darwin.dispatch_get_main_queue
+import platform.darwin.dispatch_queue_t
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExperimentalForeignApi
+import org.jetbrains.compose.resources.ExperimentalResourceApi
 
+// Modified from https://github.com/JetBrains/compose-multiplatform/blob/master/examples/imageviewer/shared/src/iosMain/kotlin/example/imageviewer/view/CameraView.ios.kt
+
+@Composable
+actual fun CameraView(onFoundPayload: (text: String) -> Unit){
+    var cameraAccess: Boolean? by remember { mutableStateOf(null) }
+    LaunchedEffect(Unit) {
+        when (AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeVideo)) {
+            AVAuthorizationStatusAuthorized -> {
+                cameraAccess = true
+            }
+
+            AVAuthorizationStatusDenied, AVAuthorizationStatusRestricted -> {
+                cameraAccess = false
+            }
+
+            AVAuthorizationStatusNotDetermined -> {
+                AVCaptureDevice.requestAccessForMediaType(
+                    mediaType = AVMediaTypeVideo
+                ) { success ->
+                    cameraAccess = success
+                }
+            }
+        }
+    }
+    Box(
+        Modifier.fillMaxSize().background(Color.Black),
+        contentAlignment = Alignment.Center
+    ) {
+        when (cameraAccess) {
+            null -> {
+                // Waiting for the user to accept permission
+            }
+
+            false -> {
+                Text("Camera access denied", color = Color.White)
+            }
+
+            true -> {
+                AuthorizedCamera(onFoundPayload)
+            }
+        }
+    }
+}
+
+private val deviceTypes = listOf(
+    AVCaptureDeviceTypeBuiltInWideAngleCamera,
+    AVCaptureDeviceTypeBuiltInDualWideCamera,
+    AVCaptureDeviceTypeBuiltInDualCamera,
+    AVCaptureDeviceTypeBuiltInUltraWideCamera,
+    AVCaptureDeviceTypeBuiltInDuoCamera
+)
+
+@Composable
+private fun BoxScope.AuthorizedCamera(onFoundPayload: (text: String) -> Unit){
+    val camera: AVCaptureDevice? = remember {
+        AVCaptureDeviceDiscoverySession.discoverySessionWithDeviceTypes(
+            deviceTypes = deviceTypes,
+            mediaType = AVMediaTypeVideo,
+            position = AVCaptureDevicePositionBack,
+        ).devices.firstOrNull() as? AVCaptureDevice
+    }
+    if (camera != null) {
+        RealDeviceCamera(camera, onFoundPayload)
+    } else {
+        Text(
+            """
+            Camera is not available on simulator.
+            Please try to run on a real iOS device.
+        """.trimIndent(), color = Color.White
+        )
+    }
+}
+
+@OptIn(ExperimentalResourceApi::class, ExperimentalForeignApi::class, BetaInteropApi::class)
+@Composable
+private fun BoxScope.RealDeviceCamera(camera: AVCaptureDevice, onFoundPayload: (text: String) -> Unit){
+
+
+    val capturePhotoOutput = remember { AVCapturePhotoOutput() }
+    var actualOrientation by remember {
+        mutableStateOf(
+            AVCaptureVideoOrientationPortrait
+        )
+    }
+
+    val metaDelegate = remember {
+        object : NSObject(), AVCaptureMetadataOutputObjectsDelegateProtocol {
+            override fun captureOutput(
+                output: platform.AVFoundation.AVCaptureOutput,
+                didOutputMetadataObjects: kotlin.collections.List<*>,
+                fromConnection: platform.AVFoundation.AVCaptureConnection
+            ) {
+                try {
+                    val readableObject = didOutputMetadataObjects.first() as AVMetadataMachineReadableCodeObject
+                    if(readableObject.stringValue != null){
+                        println(readableObject.stringValue)
+                        onFoundPayload(readableObject.stringValue.toString())
+                    }
+                } catch(e: Exception) {
+
+                }
+
+            }
+        }
+    }
+
+    val captureMetadataOutput = AVCaptureMetadataOutput()
+
+    val dispatchQueue: dispatch_queue_t = dispatch_get_main_queue()
+
+    captureMetadataOutput.setMetadataObjectsDelegate(objectsDelegate = metaDelegate, queue = dispatchQueue)
+
+    val captureSession: AVCaptureSession = remember {
+        AVCaptureSession().also { captureSession ->
+            captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+            val captureDeviceInput: AVCaptureDeviceInput =
+                AVCaptureDeviceInput.deviceInputWithDevice(device = camera, error = null)!!
+            captureSession.addInput(captureDeviceInput)
+            captureSession.addOutput(captureMetadataOutput)
+        }
+    }
+
+    captureMetadataOutput.metadataObjectTypes = listOf(AVMetadataObjectTypeQRCode)
+
+    val cameraPreviewLayer = remember {
+        AVCaptureVideoPreviewLayer(session = captureSession)
+    }
+
+    DisposableEffect(Unit) {
+        class OrientationListener : NSObject() {
+            @Suppress("UNUSED_PARAMETER")
+            @ObjCAction
+            fun orientationDidChange(arg: NSNotification) {
+                val cameraConnection = cameraPreviewLayer.connection
+                if (cameraConnection != null) {
+                    actualOrientation = when (UIDevice.currentDevice.orientation) {
+                        UIDeviceOrientation.UIDeviceOrientationPortrait ->
+                            AVCaptureVideoOrientationPortrait
+
+                        UIDeviceOrientation.UIDeviceOrientationLandscapeLeft ->
+                            AVCaptureVideoOrientationLandscapeRight
+
+                        UIDeviceOrientation.UIDeviceOrientationLandscapeRight ->
+                            AVCaptureVideoOrientationLandscapeLeft
+
+                        UIDeviceOrientation.UIDeviceOrientationPortraitUpsideDown ->
+                            AVCaptureVideoOrientationPortrait
+
+                        else -> cameraConnection.videoOrientation
+                    }
+                    cameraConnection.videoOrientation = actualOrientation
+                }
+                capturePhotoOutput.connectionWithMediaType(AVMediaTypeVideo)
+                    ?.videoOrientation = actualOrientation
+            }
+        }
+
+        val listener = OrientationListener()
+        val notificationName = UIDeviceOrientationDidChangeNotification
+        NSNotificationCenter.defaultCenter.addObserver(
+            observer = listener,
+            selector = NSSelectorFromString(
+                OrientationListener::orientationDidChange.name + ":"
+            ),
+            name = notificationName,
+            `object` = null
+        )
+        onDispose {
+            NSNotificationCenter.defaultCenter.removeObserver(
+                observer = listener,
+                name = notificationName,
+                `object` = null
+            )
+        }
+    }
+    Column {
+        Box(modifier = Modifier.background(color = Color.White).height(80.dp), contentAlignment = Alignment.TopCenter){
+            Row(Modifier.padding(10.dp).height(80.dp).fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+                Icon(Icons.Default.Close, contentDescription = null, Modifier.size(30.dp).clickable(onClick = { globalBack() }), tint = Color.LightGray)
+                Text("DemoWallet", color = MaterialTheme.colorScheme.primary, fontSize = 40.sp, fontWeight = FontWeight.Bold)
+                Icon(Icons.Default.Close, contentDescription = null, Modifier.size(30.dp).clickable(onClick = { }), tint = Color.LightGray.copy(alpha = 0.0f))
+            }
+        }
+
+        UIKitView(
+            modifier = Modifier.fillMaxSize(),
+            background = Color.Black,
+            factory = {
+                val cameraContainer = UIView()
+                cameraContainer.layer.addSublayer(cameraPreviewLayer)
+                cameraPreviewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+                captureSession.startRunning()
+                cameraContainer
+            },
+            onResize = { view: UIView, rect: CValue<CGRect> ->
+                CATransaction.begin()
+                CATransaction.setValue(true, kCATransactionDisableActions)
+                view.layer.setFrame(rect)
+                cameraPreviewLayer.setFrame(rect)
+                CATransaction.commit()
+            },
+        )
+    }
 }
