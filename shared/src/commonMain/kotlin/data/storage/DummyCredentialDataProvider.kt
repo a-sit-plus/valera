0196847c5b9819cace25b53c2e4e48c5aa737fdc
md4096 <144923189+md4096@users.noreply.github.com>
2023-11-02 13:30:00 +0100

    Cleanup

--- shared/src/commonMain/kotlin/data/storage/DummyCredentialDataProvider.kt
+++ shared/src/commonMain/kotlin/data/storage/DummyCredentialDataProvider.kt
@@ -19,6 +19,9 @@ import kotlinx.datetime.LocalDate
 import kotlin.random.Random
 import kotlin.time.Duration.Companion.minutes
 
+/**
+ * Temporary class to create random credentials
+ */
 class DummyCredentialDataProvider(
     private val clock: Clock = Clock.System
 ) : IssuerCredentialDataProvider {
@@ -66,32 +69,6 @@ class DummyCredentialDataProvider(
             )
         }
 
-        if (attributeTypes.contains(ConstantIndex.MobileDrivingLicence2023.vcType) && subjectPublicKey != null) {
-            val drivingPrivilege = DrivingPrivilege(
-                vehicleCategoryCode = "B",
-                issueDate = LocalDate.parse("2023-01-01"),
-                expiryDate = LocalDate.parse("2033-01-31"),
-                //codes = arrayOf(DrivingPrivilegeCode(code = "B"))
-            )
-            val issuerSignedItems = listOf(
-                buildIssuerSignedItem(IsoDataModelConstants.DataElements.FAMILY_NAME, "Mustermann", 0U),
-                buildIssuerSignedItem(IsoDataModelConstants.DataElements.GIVEN_NAME, "Max", 1U),
-                buildIssuerSignedItem(IsoDataModelConstants.DataElements.DOCUMENT_NUMBER, "123456789", 2U),
-                buildIssuerSignedItem(IsoDataModelConstants.DataElements.ISSUE_DATE, "2023-01-01", 3U),
-                buildIssuerSignedItem(IsoDataModelConstants.DataElements.EXPIRY_DATE, "2033-01-31", 4U),
-                //buildIssuerSignedItem(DataElements.DRIVING_PRIVILEGES, drivingPrivilege, 5U),
-            )
-
-            listOfAttributes.add(
-                CredentialToBeIssued.Iso(
-                    issuerSignedItems = issuerSignedItems,
-                    subjectPublicKey = subjectPublicKey,
-                    expiration = expiration,
-                    attributeType = at.asitplus.wallet.lib.data.ConstantIndex.MobileDrivingLicence2023.vcType,
-                )
-            )
-        }
-
         if (attributeTypes.contains(data.idaustria.ConstantIndex.IdAustriaCredential.vcType)) {
             val listFirstname = listOf("Max", "Susanne", "Peter", "Petra", "Hans", "Anna", "Martin", "Barbara")
             val listLastname = listOf("Müller", "Schmidt", "Schneider", "Fischer", "Weber", "Meyer", "Becker", "Koch")
@@ -114,19 +91,4 @@ class DummyCredentialDataProvider(
     }
 
     private fun randomValue() = Random.nextBytes(32).encodeToString(Base16(strict = true))
-
-    fun buildIssuerSignedItem(elementIdentifier: String, elementValue: String, digestId: UInt) = IssuerSignedItem(
-        digestId = digestId,
-        random = Random.nextBytes(16),
-        elementIdentifier = elementIdentifier,
-        elementValue = ElementValue(string = elementValue)
-    )
-
-    fun buildIssuerSignedItem(elementIdentifier: String, elementValue: DrivingPrivilege, digestId: UInt) =
-        IssuerSignedItem(
-            digestId = digestId,
-            random = Random.nextBytes(16),
-            elementIdentifier = elementIdentifier,
-            elementValue = ElementValue(drivingPrivilege = arrayOf(elementValue))
-        )
 }
