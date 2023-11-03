9ca3fa7f4d5e02a43c4cb83616bfc3e53ea02f3c
md4096 <144923189+md4096@users.noreply.github.com>
2023-11-03 14:36:00 +0100

    Add basic Android intent

--- androidApp/src/androidMain/kotlin/at/asitplus/wallet/app/android/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/at/asitplus/wallet/app/android/MainActivity.kt
@@ -4,6 +4,8 @@ import MainView
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +14,20 @@ class MainActivity : AppCompatActivity() {
         setContent {
             MainView()
         }
+    }
+}
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MainView()
+        }
+        // ATTENTION: This was auto-generated to handle app links.
+        val appLinkIntent: Intent = intent
+        val appLinkAction: String? = appLinkIntent.action
+        val appLinkData: Uri? = appLinkIntent.data
+        println(appLinkIntent)
     }
 }
