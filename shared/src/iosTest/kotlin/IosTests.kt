e69a05c60855a3c87bc50fd89582b85d8e034c58
md4096 <144923189+md4096@users.noreply.github.com>
2023-11-17 11:06:48 +0100

    Add iOS specific test

--- shared/src/iosTest/kotlin/IosTests.kt
+++ shared/src/iosTest/kotlin/IosTests.kt
@@ -1,13 +1,17 @@
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import io.kotest.common.runBlocking
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertIs
 class IosTests {
-    // Just an dummy test
     @Test
     fun testDataStore() {
-        val result = createDataStore()
-        assertIs<DataStore<Preferences>>(result)
+        val dataStore = createDataStore()
+        val dataStoreService = DataStoreService(dataStore)
+        runBlocking { dataStoreService.setData(Resources.DEBUG_DATASTORE_VALUE, Resources.DEBUG_DATASTORE_KEY) }
+        val result = runBlocking { dataStoreService.getData(Resources.DEBUG_DATASTORE_KEY) }
+        assertEquals(Resources.DEBUG_DATASTORE_VALUE, result)
     }
 }
