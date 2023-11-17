7fb328966ce393ede856a5c3fb9bfe817a21ced7
md4096 <144923189+md4096@users.noreply.github.com>
2023-11-17 15:02:32 +0100

    Add simple Android instrumented test

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -82,6 +82,13 @@ kotlin {
 
             }
         }
+
+        val androidInstrumentedTest by getting {
+            dependencies {
+                implementation("androidx.compose.ui:ui-test-junit4")
+                implementation("androidx.compose.ui:ui-test-manifest")
+            }
+        }
     }
 }
 
@@ -95,6 +102,7 @@ android {
 
     defaultConfig {
         minSdk = (findProperty("android.minSdk") as String).toInt()
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
