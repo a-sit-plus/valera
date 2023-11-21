5b79cf8887dd21069d6401867ce1b724c5aa02e7
md4096 <144923189+md4096@users.noreply.github.com>
2023-11-21 17:06:11 +0100

    Use map instead of creating a new one

--- shared/src/commonMain/kotlin/data/storage/DummyDataStoreService.kt
+++ shared/src/commonMain/kotlin/data/storage/DummyDataStoreService.kt
@@ -3,7 +3,7 @@ package data.storage
 class DummyDataStoreService: DataStoreService {
     var memory: MutableMap<String, String> = mutableMapOf()
     override suspend fun setData(value: String, key: String){
-        memory = mutableMapOf(key to value)
+        memory[key] = value
     }
 
     override suspend fun getData(key: String): String? {
