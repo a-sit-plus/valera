abd9947e0a6b7684935b4995406675bc795555fb
md4096 <144923189+md4096@users.noreply.github.com>
2023-11-29 17:53:28 +0100

    Start with oid4vci

--- shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/ProvisioningService.kt
+++ shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/ProvisioningService.kt
@@ -3,8 +3,15 @@ package at.asitplus.wallet.app.common
 import DataStoreService
 import at.asitplus.wallet.lib.agent.CryptoService
 import at.asitplus.wallet.lib.data.ConstantIndex
+import at.asitplus.wallet.lib.oidc.OpenIdConstants.TOKEN_PREFIX_BEARER
+import at.asitplus.wallet.lib.oidvci.CredentialResponseParameters
+import at.asitplus.wallet.lib.oidvci.IssuerMetadata
+import at.asitplus.wallet.lib.oidvci.TokenResponseParameters
 import at.asitplus.wallet.lib.oidvci.WalletService
+import at.asitplus.wallet.lib.oidvci.encodeToParameters
+import at.asitplus.wallet.lib.oidvci.formUrlEncode
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.plugins.DefaultRequest
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.cookies.AcceptAllCookiesStorage
@@ -13,13 +20,21 @@ import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.forms.submitForm
 import io.ktor.client.request.get
 import io.ktor.client.request.header
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
+import io.ktor.http.Url
+import io.ktor.http.contentType
+import io.ktor.http.headers
 import io.ktor.serialization.kotlinx.json.json
 
 const val HOST = "https://wallet.a-sit.at"
+const val PATH_WELL_KNOWN_CREDENTIAL_ISSUER = "/.well-known/openid-credential-issuer"
 
 class ProvisioningService(val objectFactory: ObjectFactory, val dataStoreService: DataStoreService, val cryptoService: CryptoService) {
     var xauth: String? = null
@@ -70,13 +85,48 @@ class ProvisioningService(val objectFactory: ObjectFactory, val dataStoreService
     }
 
     suspend fun step4(){
+        val metadata: IssuerMetadata = client.get("https://eid.a-sit.at/wallet$PATH_WELL_KNOWN_CREDENTIAL_ISSUER").body()
+
         val oid4vciService = WalletService(
             credentialScheme = at.asitplus.wallet.idaustria.ConstantIndex.IdAustriaCredential,
             clientId = "https://eid.a-sit.at/wallet",
             cryptoService = cryptoService,
             credentialRepresentation = ConstantIndex.CredentialRepresentation.PLAIN_JWT
         )
+
         val authRequest = oid4vciService.createAuthRequest()
-        println("authRequest: $authRequest")
+
+        val codeUrl = client.get(metadata.authorizationEndpointUrl) {
+            authRequest.encodeToParameters().forEach {
+                this.parameter(it.key, it.value)
+            }
+        }.headers[HttpHeaders.Location]
+
+        if (codeUrl == null) {
+            TODO("")
+        }
+        val code = Url(codeUrl).parameters["code"]
+        if (code == null) {
+           TODO("")
+        }
+
+        val tokenRequest = oid4vciService.createTokenRequestParameters(code)
+        val tokenResponse: TokenResponseParameters = client.submitForm(metadata.tokenEndpointUrl!!) {
+            setBody(tokenRequest.encodeToParameters().formUrlEncode())
+        }.body()
+        val credentialRequest = oid4vciService.createCredentialRequest(tokenResponse, metadata)
+        val credentialResponse: CredentialResponseParameters =
+            client.post(metadata.credentialEndpointUrl!!) {
+                contentType(ContentType.Application.Json)
+                setBody(credentialRequest)
+                headers {
+                    append(
+                        HttpHeaders.Authorization,
+                        "$TOKEN_PREFIX_BEARER${tokenResponse.accessToken}"
+                    )
+                }
+            }.body()
+        println(credentialResponse)
+
     }
 }
