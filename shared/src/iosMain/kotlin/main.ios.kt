3a22226c45801c09ff0cb32f81a63c002d748b17
md4096 <144923189+md4096@users.noreply.github.com>
2023-12-04 13:54:58 +0100

    Add image decoding to iOS

--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -1,14 +1,15 @@
-
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.window.ComposeUIViewController
 import at.asitplus.wallet.app.common.ObjectFactory
 import at.asitplus.wallet.app.common.PlatformAdapter
 import at.asitplus.wallet.app.common.WalletMain
+import org.jetbrains.skia.Image
 
 actual fun getPlatformName(): String = "iOS"
 
@@ -21,7 +22,7 @@ actual fun getColorScheme(): ColorScheme{
     }
 }
 actual fun decodeImage(image: ByteArray): ImageBitmap {
-    TODO("Return ImageBitmap from JPEG ByteArray")
+    return Image.makeFromEncoded(image).toComposeImageBitmap()
 }
 
 
