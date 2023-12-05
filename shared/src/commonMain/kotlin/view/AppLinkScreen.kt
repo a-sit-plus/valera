222aafcab00b136cb098d3745662d81fd88abc74
md4096 <144923189+md4096@users.noreply.github.com>
2023-12-05 17:20:45 +0100

    Add loading animation

--- shared/src/commonMain/kotlin/view/AppLinkScreen.kt
+++ shared/src/commonMain/kotlin/view/AppLinkScreen.kt
@@ -3,68 +3,49 @@ package view
 import Resources
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import appLink
 import at.asitplus.wallet.app.common.WalletMain
+import io.github.aakira.napier.Napier
 import kotlinx.coroutines.runBlocking
 
 @Composable
-fun AppLinkScreen(onContinueClick: () -> Unit, walletMain: WalletMain){
-    println("Redirect: ${appLink.value}")
-    var url = appLink.value ?: Resources.UNKNOWN
-    if (url.length > 100) {
-        url = url.subSequence(0, 99).toString()
-    }
+fun AppLinkScreen(walletMain: WalletMain){
+    Napier.d("Redirect: ${appLink.value}")
     Column(
         modifier = Modifier.fillMaxSize().background(color = MaterialTheme.colorScheme.primaryContainer),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(Resources.COMPOSE_WALLET, fontSize = 24.sp, fontWeight = FontWeight.Bold, color = MaterialTheme.colorScheme.primary)
-        Text(Resources.REDIRECT_TITLE, fontSize = 24.sp, fontWeight = FontWeight.Bold, color = MaterialTheme.colorScheme.primary)
 
-        Box(Modifier.fillMaxWidth().padding(20.dp)){
-            Box(Modifier.clip(shape = RoundedCornerShape(10.dp)).background(color = Color.White).fillMaxWidth().padding(20.dp)){
-                Column {
-                    Text(Resources.URL + " : " + url)
-                }
-
+        CircularProgressIndicator(
+            modifier = Modifier.width(100.dp),
+            color = MaterialTheme.colorScheme.secondary,
+            trackColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
+        runBlocking {
+            if (appLink.value?.contains("https://wallet.a-sit.at/m1/login/oauth2/code/idaq?code=") == true) {
+                walletMain.provisioningService.step3(appLink.value!!)
+            }
+            if (appLink.value == "https://wallet.a-sit.at/m1/") {
+                walletMain.provisioningService.step4()
+            }
+            if (appLink.value?.contains("https://wallet.a-sit.at/mobile") == true) {
+                walletMain.presentationService.startSiop(appLink.value!!)
             }
         }
-        Button(
-            modifier = Modifier
-                .padding(vertical = 24.dp),
-            onClick = {
-                if (appLink.value?.contains("https://wallet.a-sit.at/m1/login/oauth2/code/idaq?code=") == true) {
-                    runBlocking { walletMain.provisioningService.step3(appLink.value!!) }
-                }
-                if (appLink.value == "https://wallet.a-sit.at/m1/") {
-                    runBlocking { walletMain.provisioningService.step4() }
-                }
-                if (appLink.value?.contains("https://wallet.a-sit.at/mobile") == true) {
-                    runBlocking { walletMain.presentationService.startSiop(appLink.value!!) }
-                }
-                onContinueClick()
-                appLink.value = null
-            }
-        ) {
-            Text(Resources.BUTTON_CONTINUE)
-        }
+        appLink.value = null
     }
 }
