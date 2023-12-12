d458699979f95470160ce04a6610a5f2d844e166
md4096 <144923189+md4096@users.noreply.github.com>
2023-12-12 11:17:54 +0100

    Debug info with correct class name

--- shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/PresentationService.kt
+++ shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/PresentationService.kt
@@ -8,16 +8,16 @@ import io.github.aakira.napier.Napier
 
 class PresentationService(val platformAdapter: PlatformAdapter, val dataStoreService: DataStoreService, val cryptoService: CryptoService, val holderAgent: HolderAgent) {
     suspend fun startSiop(url: String){
-        Napier.d("ProvisioningService: [startSiop] Start SIOP process")
+        Napier.d("PresentationService: [startSiop] Start SIOP process")
         val oidcSiopWallet = OidcSiopWallet.newInstance(
             holder = holderAgent,
             cryptoService = cryptoService,
         )
         val authenticationResponse = oidcSiopWallet.createAuthnResponse(url)
         if (authenticationResponse.isFailure) {
-            throw Exception("ProvisioningService: [startSiop] Failure in received authentication response")
+            throw Exception("PresentationService: [startSiop] Failure in received authentication response")
         }
-        Napier.d("ProvisioningService: [startSiop] Opening $authenticationResponse")
+        Napier.d("PresentationService: [startSiop] Opening $authenticationResponse")
         when (val response= authenticationResponse.getOrThrow()){
             is OidcSiopWallet.AuthenticationResponseResult.Post -> TODO()
             is OidcSiopWallet.AuthenticationResponseResult.Redirect -> platformAdapter.openUrl(response.url)
