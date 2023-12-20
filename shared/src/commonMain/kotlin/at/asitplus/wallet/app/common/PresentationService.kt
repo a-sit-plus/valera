0dbb4bbc9dce9fbd27f4f966ccb71c80286e8bd9
md4096 <144923189+md4096@users.noreply.github.com>
2023-12-20 16:50:36 +0100

    Add post functionality in PresentationService

--- shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/PresentationService.kt
+++ shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/PresentationService.kt
@@ -4,9 +4,44 @@ import DataStoreService
 import at.asitplus.wallet.lib.agent.CryptoService
 import at.asitplus.wallet.lib.agent.HolderAgent
 import at.asitplus.wallet.lib.oidc.OidcSiopWallet
+import data.storage.PersistentCookieStorage
 import io.github.aakira.napier.Napier
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.cookies.HttpCookies
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.serialization.kotlinx.json.json
 
 class PresentationService(val platformAdapter: PlatformAdapter, val dataStoreService: DataStoreService, val cryptoService: CryptoService, val holderAgent: HolderAgent) {
+    private val cookieStorage = PersistentCookieStorage(dataStoreService)
+    private val client = HttpClient {
+        followRedirects = false
+        install(ContentNegotiation) {
+            json()
+        }
+
+        install(DefaultRequest) {
+            header(HttpHeaders.ContentType, ContentType.Application.Json)
+        }
+
+        install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.ALL
+        }
+        install(HttpCookies) {
+            storage = cookieStorage
+        }
+    }
+
     @Throws(Throwable::class)
     suspend fun startSiop(url: String){
         Napier.d("Start SIOP process")
@@ -21,7 +56,11 @@ class PresentationService(val platformAdapter: PlatformAdapter, val dataStoreSer
         Napier.d("Opening $authenticationResponse")
         when (val response= authenticationResponse.getOrThrow()){
             is OidcSiopWallet.AuthenticationResponseResult.Post -> {
-                TODO("Function not implemented")
+                val tests = client.post(response.url) {
+                    Napier.d("Set Body: ${response.content}")
+                    setBody(response.content)
+                }
+                Napier.d("Response: $tests")
             }
             is OidcSiopWallet.AuthenticationResponseResult.Redirect -> platformAdapter.openUrl(response.url)
         }
