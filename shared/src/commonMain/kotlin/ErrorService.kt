f0c9f5adb4f90dcaaa9c0b3eb12e4a780a7188e3
md4096 <144923189+md4096@users.noreply.github.com>
2024-01-11 16:32:27 +0100

    Add Napier error to ErrorService

--- shared/src/commonMain/kotlin/ErrorService.kt
+++ shared/src/commonMain/kotlin/ErrorService.kt
@@ -1,12 +1,17 @@
 import androidx.compose.runtime.MutableState
+import io.github.aakira.napier.Napier
 
 class ErrorService(var showError: MutableState<Boolean>, var errorText: MutableState<String>) {
     fun emit (e: Exception){
         if (e.message == null) {
             errorText.value = "Unknown Exception"
+            Napier.e("Unknown Exception")
+            Napier.e("StackTrace: " + e.stackTraceToString())
             showError.value = true
         } else {
             errorText.value = e.message.toString()
+            Napier.e(e.message.toString())
+            Napier.e("StackTrace: " + e.stackTraceToString())
             showError.value = true
         }
     }
