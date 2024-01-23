ca4fa2bd7d414b300f0801f59ee60ea0f36159a1
md4096 <144923189+md4096@users.noreply.github.com>
2024-01-23 12:53:34 +0100

    Change keyboardType to Uri

--- shared/src/commonMain/kotlin/view/AboutScreen.kt
+++ shared/src/commonMain/kotlin/view/AboutScreen.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import at.asitplus.wallet.app.common.WalletMain
