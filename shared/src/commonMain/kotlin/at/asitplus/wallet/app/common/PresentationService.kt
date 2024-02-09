25f3fd7865d2ad9fc09f989d4e99104063a92e1d
md4096 <144923189+md4096@users.noreply.github.com>
2024-02-09 13:54:51 +0100

    Catch InternalServerError from SP

--- shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/PresentationService.kt
+++ shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/PresentationService.kt
@@ -18,8 +18,10 @@ import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
+import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.kotlinx.json.json
 
 class PresentationService(
@@ -65,6 +67,9 @@ class PresentationService(
                         Napier.d("Post $authenticationResponse")
                         val response = client.post(it.url) {
                             setBody(it.content)
+                        }
+                        if (response.status == HttpStatusCode.InternalServerError) {
+                            throw  Exception("InternalServerErrorException", Exception(response.bodyAsText()))
                         }
                         val location = response.headers["Location"]
                         if (location != null) {
