1d908c054d83ab17da0b2b3d2c052bfdb57ba9a3
acrusage <121168839+acrusage@users.noreply.github.com>
2024-02-28 16:16:31 +0100

    Remove: Remove trivial instrumented test as it doesn't apply anymore

--- shared/src/androidInstrumentedTest/kotlin/InstrumentedTests.kt
+++ shared/src/androidInstrumentedTest/kotlin/InstrumentedTests.kt
@@ -1,12 +1,6 @@
 
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
-import at.asitplus.wallet.app.common.DummyPlatformAdapter
-import at.asitplus.wallet.app.common.WalletMain
-import data.storage.DummyDataStoreService
 import org.junit.Rule
-import org.junit.Test
 
 // Modified from https://developer.android.com/jetpack/compose/testing
 class InstrumentedTests {
@@ -16,14 +10,18 @@ class InstrumentedTests {
     // use createAndroidComposeRule<YourActivity>() if you need access to
     // an activity
 
-    @Test
-    fun test1() {
-        // Start the app
-        composeTestRule.setContent {
-            App(
-                WalletMain(objectFactory = AndroidObjectFactory(), dataStoreService = DummyDataStoreService(), platformAdapter = DummyPlatformAdapter())
-            )
-        }
-        composeTestRule.onNodeWithText(Resources.WALLET).assertIsDisplayed()
-    }
+
+// test does not really apply anymore, the app starts with an empty screen until data store has been accessed
+//    @Test
+//    fun test1() {
+//        // Start the app
+//        composeTestRule.setContent {
+//            val walletMain = WalletMain(objectFactory = AndroidObjectFactory(), dataStoreService = DummyDataStoreService(), platformAdapter = DummyPlatformAdapter())
+//            App(walletMain)
+//            runBlocking {
+//                walletMain.resetApp()
+//            }
+//        }
+//        composeTestRule.onNodeWithText(Resources.BUTTON_LABEL_START).assertIsDisplayed()
+//    }
 }
