52ba0fa56a672049b6071186fa812fba51bffbca
acrusage <121168839+acrusage@users.noreply.github.com>
2024-03-06 18:21:19 +0100

    Change: Move QR-Code Scanning logic into domain layer

---
+++ shared/src/commonMain/kotlin/domain/ExtractRequestObjectFromRedirectUriUseCase.kt
@@ -0,0 +1,50 @@
+package domain
+
+import Resources
+import at.asitplus.wallet.lib.jws.JwsSigned
+import at.asitplus.wallet.lib.jws.VerifierJwsService
+import at.asitplus.wallet.lib.oidc.AuthenticationRequestParameters
+import at.asitplus.wallet.lib.oidc.OpenIdConstants
+import at.asitplus.wallet.lib.oidvci.OAuth2Exception
+import at.asitplus.wallet.lib.oidvci.decodeFromUrlQuery
+import io.github.aakira.napier.Napier
+import io.ktor.http.Url
+import io.ktor.util.flattenEntries
+
+class ExtractRequestObjectFromRedirectUriUseCase(
+    private val verifierJwsService: VerifierJwsService,
+) {
+
+    operator fun invoke(requestRedirectUri: String): AuthenticationRequestParameters {
+        val requestParams = kotlin.runCatching {
+            Url(requestRedirectUri).parameters.flattenEntries().toMap()
+                .decodeFromUrlQuery<AuthenticationRequestParameters>()
+        }.getOrNull()
+            ?: throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
+                .also { Napier.w("Could not parse authentication request") }
+
+        val requestLocationClientId = requestParams.clientId
+
+        val requestLocationRequestParameterParsed = requestParams.let { extractRequestObject(it) ?: it }
+        if (requestLocationRequestParameterParsed.clientId != requestLocationClientId) {
+            throw Exception("${Resources.ERROR_QR_CODE_SCANNING_INCONSISTENT_CLIENT_ID}: UrlParameter: $requestLocationClientId, AuthenticationRequestParameters: ${requestLocationRequestParameterParsed.clientId}")
+        }
+        return requestLocationRequestParameterParsed
+    }
+
+    // copied from OidcSiopWallet
+    private fun extractRequestObject(params: AuthenticationRequestParameters): AuthenticationRequestParameters? {
+        params.request?.let { requestObject ->
+            JwsSigned.parse(requestObject)?.let { jws ->
+                if (verifierJwsService.verifyJwsObject(jws, requestObject)) {
+                    return kotlin.runCatching {
+                        at.asitplus.wallet.lib.oidc.jsonSerializer.decodeFromString<AuthenticationRequestParameters>(
+                            jws.payload.decodeToString()
+                        )
+                    }.getOrNull()
+                }
+            }
+        }
+        return null
+    }
+}
--- shared/src/commonMain/kotlin/view/AuthenticationQrCodeScannerViewModel.kt
+++ shared/src/commonMain/kotlin/view/AuthenticationQrCodeScannerViewModel.kt
@@ -1,20 +1,12 @@
 package view
 
 import Resources
-import at.asitplus.wallet.app.common.HttpService
-import at.asitplus.wallet.lib.data.jsonSerializer
-import at.asitplus.wallet.lib.jws.DefaultVerifierJwsService
-import at.asitplus.wallet.lib.jws.JwsSigned
-import at.asitplus.wallet.lib.oidc.AuthenticationRequestParameters
-import at.asitplus.wallet.lib.oidc.RelyingPartyMetadata
-import at.asitplus.wallet.lib.oidvci.decodeFromUrlQuery
+import domain.ExtractRequestObjectFromRedirectUriUseCase
+import domain.RetrieveRelyingPartyMetadataFromAuthenticationQrCodeUseCase
+import domain.RetrieveRequestRedirectFromAuthenticationQrCodeUseCase
+import domain.ValidateClientMetadataAndRequestParameterConsistencyUseCase
 import io.github.aakira.napier.Napier
-import io.ktor.client.request.get
-import io.ktor.client.statement.bodyAsText
-import io.ktor.http.HttpHeaders
-import io.ktor.http.Url
 import io.ktor.http.parseQueryString
-import io.ktor.util.flattenEntries
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -22,7 +14,12 @@ import kotlinx.coroutines.IO
 import kotlinx.coroutines.launch
 import navigation.AuthenticationConsentPage
 
-class AuthenticationQrCodeScannerViewModel {
+class AuthenticationQrCodeScannerViewModel(
+    private val retrieveRelyingPartyMetadataFromAuthenticationQrCodeUseCase: RetrieveRelyingPartyMetadataFromAuthenticationQrCodeUseCase,
+    private val retrieveRequestRedirectFromAuthenticationQrCodeUseCase: RetrieveRequestRedirectFromAuthenticationQrCodeUseCase,
+    private val extractRequestObjectFromRedirectUriUseCase: ExtractRequestObjectFromRedirectUriUseCase,
+    private val validateClientMetadataAndRequestParameterConsistencyUseCase: ValidateClientMetadataAndRequestParameterConsistencyUseCase,
+) {
     fun onScan(
         link: String,
         startLoadingCallback: () -> Unit,
@@ -30,6 +27,8 @@ class AuthenticationQrCodeScannerViewModel {
         onSuccess: (AuthenticationConsentPage) -> Unit,
         onFailure: (Throwable) -> Unit,
     ) {
+        Napier.d("onScan: $link")
+
         startLoadingCallback()
         val coroutineExceptionHandler = CoroutineExceptionHandler { _, error ->
             // Do what you want with the error
@@ -38,74 +37,20 @@ class AuthenticationQrCodeScannerViewModel {
         }
 
         CoroutineScope(Dispatchers.IO).launch(coroutineExceptionHandler) {
-            Napier.d("onScan: $link")
+            val clientMetadataPayload = retrieveRelyingPartyMetadataFromAuthenticationQrCodeUseCase(link)
+            val redirectUri = retrieveRequestRedirectFromAuthenticationQrCodeUseCase(link)
+            val authenticationRequestParameters = extractRequestObjectFromRedirectUriUseCase(redirectUri)
+            validateClientMetadataAndRequestParameterConsistencyUseCase(
+                clientMetadataPayload = clientMetadataPayload,
+                authenticationRequestParameters = authenticationRequestParameters,
+            )
 
             val parameterIndex = link.indexOfFirst { it == '?' }
             val linkParams = parseQueryString(link, startIndex = parameterIndex + 1)
-
-            val requestUri = linkParams["request_uri"]
-                ?: throw Exception("${Resources.ERROR_QR_CODE_SCANNING_MISSING_REQUEST_URI}: $link")
-
-            val metadataUri = linkParams["metadata_uri"] ?: linkParams["client_metadata_uri"]
-            ?: throw Exception("${Resources.ERROR_QR_CODE_SCANNING_MISSING_CLIENT_METADATA}: $link")
-
-            val client = HttpService().buildHttpClient()
-            val verifierJwsService = DefaultVerifierJwsService()
-
-            val (redirectUri, authenticationRequestParameters) = run {
-                val requestResponse = client.get(requestUri)
-
-                val requestRedirectUri =
-                    requestResponse.headers[HttpHeaders.Location].also { Napier.d("Redirect location: $it") }
-                        ?: throw Exception("${Resources.ERROR_QR_CODE_SCANNING_MISSING_REDIRECT_LOCATION}: $requestResponse")
-
-                val requestParams = kotlin.runCatching {
-                    Url(requestRedirectUri).parameters.flattenEntries().toMap()
-                        .decodeFromUrlQuery<AuthenticationRequestParameters>()
+            val scannedLinkClientId = linkParams["client_id"]?.also {
+                if(it != authenticationRequestParameters.clientId) {
+                    throw Exception("${Resources.ERROR_QR_CODE_SCANNING_INCONSISTENT_CLIENT_ID}: ScannedLink: $it; RequestObject: ${authenticationRequestParameters.clientId}")
                 }
-
-                val requestLocationClientId = requestParams.getOrNull()?.clientId
-                val requestLocationRequestParameter = requestParams.getOrNull()?.request
-                    ?: throw Exception("${Resources.ERROR_QR_CODE_SCANNING_MISSING_REQUEST_OBJECT_PARAMETER}: $requestRedirectUri")
-
-                val requestLocationRequestParameterJws = try {
-                    JwsSigned.parse(requestLocationRequestParameter) ?: throw Exception()
-                } catch (error: Throwable) {
-                    throw Exception("${Resources.ERROR_QR_CODE_SCANNING_INVALID_REQUEST_JWS_OBJECT}: $requestLocationRequestParameter")
-                }
-
-                if (!verifierJwsService.verifyJwsObject(requestLocationRequestParameterJws, null)) {
-                    throw Exception("${Resources.ERROR_QR_CODE_SCANNING_INVALID_REQUEST_JWS_OBJECT_SIGNATURE}: $requestLocationRequestParameter")
-                }
-
-                val requestLocationRequestParameterParsed =
-                    jsonSerializer.decodeFromString<AuthenticationRequestParameters>(
-                        requestLocationRequestParameterJws.payload.decodeToString()
-                    )
-                if (requestLocationRequestParameterParsed.clientId != requestLocationClientId) {
-                    throw Exception("${Resources.ERROR_QR_CODE_SCANNING_INCONSISTENT_CLIENT_ID}: UrlParameter: $requestLocationClientId, AuthenticationRequestParameters: ${requestLocationRequestParameterParsed.clientId}")
-                }
-                RequestResponse(
-                    redirectUri = requestRedirectUri,
-                    requestParameters = requestLocationRequestParameterParsed,
-                )
-            }
-
-            val clientMetadataPayload = run {
-                val metadataResponse = client.get(metadataUri)
-                val metadataJws = try {
-                    JwsSigned.parse(metadataResponse.bodyAsText()) ?: throw Exception()
-                } catch (error: Throwable) {
-                    throw Exception("${Resources.ERROR_QR_CODE_SCANNING_INVALID_METADATA_JWS_OBJECT}: ${metadataResponse.bodyAsText()}")
-                }
-
-                if (!verifierJwsService.verifyJwsObject(metadataJws, null)) {
-                    throw Exception("${Resources.ERROR_QR_CODE_SCANNING_INVALID_METADATA_JWS_OBJECT_SIGNATURE}: ${metadataResponse.bodyAsText()}")
-                }
-
-                // metadata has been verified
-                Napier.d("metadataJws payload: ${metadataJws.payload.decodeToString()}")
-                jsonSerializer.decodeFromString<RelyingPartyMetadata>(metadataJws.payload.decodeToString())
             }
 
             val requestedClaims =
@@ -126,16 +71,6 @@ class AuthenticationQrCodeScannerViewModel {
             )
             Napier.d("requested claims: ${requestedClaims.joinToString(", ")}")
 
-            if (!clientMetadataPayload.redirectUris.contains(authenticationRequestParameters.clientId)) {
-                val redirectUris = clientMetadataPayload.redirectUris.joinToString("\n - ")
-                val message =
-                    "${Resources.ERROR_QR_CODE_SCANNING_CLIENT_ID_NOT_IN_REDICECT_URIS}:" +
-                            " ${authenticationRequestParameters.clientId} not in: \n$redirectUris)"
-                throw Exception(message)
-            } else {
-                Napier.d("Valid client id: ${authenticationRequestParameters.clientId}")
-            }
-
             stopLoadingCallback()
             // TODO("extract recipient name from the metadataResponse; the data is not yet being delivered though")
             onSuccess(
@@ -143,7 +78,7 @@ class AuthenticationQrCodeScannerViewModel {
                     url = redirectUri,
                     claims = requestedClaims,
                     recipientName = "DemoService",
-                    recipientLocation = linkParams["client_id"] ?: "DemoLocation",
+                    recipientLocation = scannedLinkClientId ?: "DemoLocation",
                     fromQrCodeScanner = true,
                 )
             )
