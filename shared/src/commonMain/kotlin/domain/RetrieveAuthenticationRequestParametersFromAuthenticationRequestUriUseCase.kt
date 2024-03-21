3b5577ca7c3ca4f8ab0ee440a8e9ebb13886fab0
acrusage <139230478+acrusage@users.noreply.github.com>
2024-03-21 12:31:42 +0100

    Fix: Also support our terminal_sp qr-code again

--- shared/src/commonMain/kotlin/domain/RetrieveAuthenticationRequestParametersFromAuthenticationRequestUriUseCase.kt
+++ shared/src/commonMain/kotlin/domain/RetrieveAuthenticationRequestParametersFromAuthenticationRequestUriUseCase.kt
@@ -10,6 +10,7 @@ import io.github.aakira.napier.Napier
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpHeaders
 import io.ktor.http.Url
 import io.ktor.util.flattenEntries
 
@@ -18,18 +19,9 @@ class RetrieveAuthenticationRequestParametersFromAuthenticationRequestUriUseCase
     private val verifierJwsService: VerifierJwsService,
 ) {
     suspend operator fun invoke(authenticationRequestUri: String): AuthenticationRequestParameters {
-        val requestParams = kotlin.runCatching {
-            val params = Url(authenticationRequestUri).parameters.flattenEntries().toMap()
+        val params = kotlin.runCatching {
+            Url(authenticationRequestUri).parameters.flattenEntries().toMap()
                 .decodeFromUrlQuery<AuthenticationRequestParameters>()
-
-            Napier.d("params: $params")
-            params.requestUri?.let {
-                this.invoke(it).also { newParams ->
-                    if (params.clientId != newParams.clientId) {
-                        throw Exception("Client ids are inconsistent: before: $params, after: $newParams")
-                    }
-                }
-            } ?: params
         }.getOrElse {
             val urlParts = authenticationRequestUri.split("/")
             Napier.d("urlParts: $urlParts")
@@ -41,10 +33,25 @@ class RetrieveAuthenticationRequestParametersFromAuthenticationRequestUriUseCase
                 Napier.d("requestJwt: $requestJwt")
                 extractJwtPayload(requestJwt) ?: throw Exception("response: $response")
             } else {
-                null
+                // currently supported for the requestUri:
+                // - use request object from redirect location
+                // - use request object in body
+                val response = client.get(authenticationRequestUri)
+                response.headers[HttpHeaders.Location]?.let {
+                    this.invoke(it)
+                } ?: extractJwtPayload(response.bodyAsText())
             }
         } ?: throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
             .also { Napier.w("Could not parse authentication request: $authenticationRequestUri") }
+
+        Napier.d("params: $params")
+        val requestParams = params.requestUri?.let {
+            this.invoke(it).also { newParams ->
+                if (params.clientId != newParams.clientId) {
+                    throw Exception("Client ids are inconsistent: before: $params, after: $newParams")
+                }
+            }
+        } ?: params
 
         val requestLocationClientId = requestParams.clientId
 
