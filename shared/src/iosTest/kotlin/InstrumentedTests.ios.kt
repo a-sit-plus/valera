58e0cc77a1824b8abef69cfa33d0deaac411dc70
kathrin.resek <kathrin.resek@a-sit.at>
2024-06-25 10:36:26 +0200

    Add platformAdapter and objectFactory methods for ios ui testing

--- shared/src/iosTest/kotlin/InstrumentedTests.ios.kt
+++ shared/src/iosTest/kotlin/InstrumentedTests.ios.kt
@@ -1,10 +1,62 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.ImageBitmap
+import at.asitplus.KmmResult
+import at.asitplus.wallet.app.common.HolderKeyService
 import at.asitplus.wallet.app.common.ObjectFactory
 import at.asitplus.wallet.app.common.PlatformAdapter
+import at.asitplus.wallet.lib.agent.CryptoService
 
 actual fun getObjectFactory(): ObjectFactory {
-    TODO("Not yet implemented")
+    return DummySwiftObjectFactory()
 }
 
+@Composable
 actual fun getPlatformAdapter(): PlatformAdapter {
-    TODO("Not yet implemented")
+    return DummySwiftPlatformAdapter()
+}
+
+class DummySwiftObjectFactory : ObjectFactory {
+    private val keyChainService: RealKeyChainService by lazy { RealKeyChainService() }
+
+    override fun loadCryptoService(): KmmResult<CryptoService> {
+        return try {
+            keyChainService.initialize()
+            val cryptoService = VcLibCryptoServiceCryptoKit(keyChainService = keyChainService)
+                ?: return KmmResult.failure(Throwable("Error on creating VcLibCryptoServiceCryptoKit"))
+            KmmResult.success(cryptoService)
+        } catch (e: Exception) {
+            KmmResult.failure(Throwable("Error from keyChainService.generateKeyPair"))
+        }
+    }
+
+    override fun loadHolderKeyService(): KmmResult<HolderKeyService> {
+        return KmmResult.success(keyChainService)
+    }
+}
+
+class DummySwiftPlatformAdapter() : PlatformAdapter {
+    override fun openUrl(url: String) {
+
+    }
+
+    override fun decodeImage(image: ByteArray): ImageBitmap {
+        return IosUtilities.decodeImage(image)
+    }
+
+    override fun writeToFile(text: String, fileName: String, folderName: String) {
+    }
+
+    override fun readFromFile(fileName: String, folderName: String): String? {
+        return null
+    }
+
+    override fun clearFile(fileName: String, folderName: String) {
+    }
+
+    override fun exitApp() {
+    }
+
+    override fun shareLog() {
+    }
+
 }
