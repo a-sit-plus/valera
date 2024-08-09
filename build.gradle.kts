90d86f0db3b1f0c69848e286c964ebb99d800f54
acrusage-iaik <139230478+acrusage-iaik@users.noreply.github.com>
2024-08-09 14:18:57 +0200

    Fix: Remove plugin application as it is already done by conventions?

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply false
 }
 
 repositories {
