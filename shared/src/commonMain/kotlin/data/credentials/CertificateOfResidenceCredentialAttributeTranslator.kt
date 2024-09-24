fe4c082b4abcba89d041aaf1458f7f912fe1fd9a
Christian Kollmann <christian.kollmann@a-sit.at>
2024-09-24 10:54:12 +0200

    Reduce code complexity

--- shared/src/commonMain/kotlin/data/credentials/CertificateOfResidenceCredentialAttributeTranslator.kt
+++ shared/src/commonMain/kotlin/data/credentials/CertificateOfResidenceCredentialAttributeTranslator.kt
@@ -2,21 +2,7 @@ package data.credentials
 
 import at.asitplus.jsonpath.core.NormalizedJsonPath
 import at.asitplus.jsonpath.core.NormalizedJsonPathSegment
-import at.asitplus.wallet.cor.CertificateOfResidenceDataElements.ADMINISTRATIVE_NUMBER
-import at.asitplus.wallet.cor.CertificateOfResidenceDataElements.ARRIVAL_DATE
-import at.asitplus.wallet.cor.CertificateOfResidenceDataElements.BIRTH_DATE
-import at.asitplus.wallet.cor.CertificateOfResidenceDataElements.BIRTH_PLACE
-import at.asitplus.wallet.cor.CertificateOfResidenceDataElements.DOCUMENT_NUMBER
-import at.asitplus.wallet.cor.CertificateOfResidenceDataElements.EXPIRY_DATE
-import at.asitplus.wallet.cor.CertificateOfResidenceDataElements.FAMILY_NAME
-import at.asitplus.wallet.cor.CertificateOfResidenceDataElements.GENDER
-import at.asitplus.wallet.cor.CertificateOfResidenceDataElements.GIVEN_NAME
-import at.asitplus.wallet.cor.CertificateOfResidenceDataElements.ISSUANCE_DATE
-import at.asitplus.wallet.cor.CertificateOfResidenceDataElements.ISSUING_AUTHORITY
-import at.asitplus.wallet.cor.CertificateOfResidenceDataElements.ISSUING_COUNTRY
-import at.asitplus.wallet.cor.CertificateOfResidenceDataElements.ISSUING_JURISDICTION
-import at.asitplus.wallet.cor.CertificateOfResidenceDataElements.NATIONALITY
-import at.asitplus.wallet.cor.CertificateOfResidenceDataElements.RESIDENCE_ADDRESS
+import at.asitplus.wallet.cor.CertificateOfResidenceDataElements
 import composewalletapp.shared.generated.resources.Res
 import composewalletapp.shared.generated.resources.attribute_friendly_name_administrative_number
 import composewalletapp.shared.generated.resources.attribute_friendly_name_arrival_date
@@ -38,28 +24,30 @@ import org.jetbrains.compose.resources.StringResource
 
 object CertificateOfResidenceCredentialAttributeTranslator : CredentialAttributeTranslator {
     override fun translate(attributeName: NormalizedJsonPath): StringResource? =
-        when (val first = attributeName.segments.firstOrNull()) {
-            is NormalizedJsonPathSegment.NameSegment -> when (first.memberName) {
-                ADMINISTRATIVE_NUMBER -> Res.string.attribute_friendly_name_administrative_number
-                ISSUANCE_DATE -> Res.string.attribute_friendly_name_issue_date
-                EXPIRY_DATE -> Res.string.attribute_friendly_name_expiry_date
-                ISSUING_AUTHORITY -> Res.string.attribute_friendly_name_issuing_authority
-                DOCUMENT_NUMBER -> Res.string.attribute_friendly_name_document_number
-                ISSUING_COUNTRY -> Res.string.attribute_friendly_name_issuing_country
-                ISSUING_JURISDICTION -> Res.string.attribute_friendly_name_issuing_jurisdiction
+        with(CertificateOfResidenceDataElements) {
+            when (val first = attributeName.segments.firstOrNull()) {
+                is NormalizedJsonPathSegment.NameSegment -> when (first.memberName) {
+                    ADMINISTRATIVE_NUMBER -> Res.string.attribute_friendly_name_administrative_number
+                    ISSUANCE_DATE -> Res.string.attribute_friendly_name_issue_date
+                    EXPIRY_DATE -> Res.string.attribute_friendly_name_expiry_date
+                    ISSUING_AUTHORITY -> Res.string.attribute_friendly_name_issuing_authority
+                    DOCUMENT_NUMBER -> Res.string.attribute_friendly_name_document_number
+                    ISSUING_COUNTRY -> Res.string.attribute_friendly_name_issuing_country
+                    ISSUING_JURISDICTION -> Res.string.attribute_friendly_name_issuing_jurisdiction
 
-                FAMILY_NAME -> Res.string.attribute_friendly_name_firstname
-                GIVEN_NAME -> Res.string.attribute_friendly_name_lastname
-                BIRTH_DATE -> Res.string.attribute_friendly_name_date_of_birth
-                RESIDENCE_ADDRESS -> Res.string.attribute_friendly_name_residence_address
-                GENDER -> Res.string.attribute_friendly_name_sex
-                BIRTH_PLACE -> Res.string.attribute_friendly_name_birth_place
-                ARRIVAL_DATE -> Res.string.attribute_friendly_name_arrival_date
-                NATIONALITY -> Res.string.attribute_friendly_name_nationality
+                    FAMILY_NAME -> Res.string.attribute_friendly_name_firstname
+                    GIVEN_NAME -> Res.string.attribute_friendly_name_lastname
+                    BIRTH_DATE -> Res.string.attribute_friendly_name_date_of_birth
+                    RESIDENCE_ADDRESS -> Res.string.attribute_friendly_name_residence_address
+                    GENDER -> Res.string.attribute_friendly_name_sex
+                    BIRTH_PLACE -> Res.string.attribute_friendly_name_birth_place
+                    ARRIVAL_DATE -> Res.string.attribute_friendly_name_arrival_date
+                    NATIONALITY -> Res.string.attribute_friendly_name_nationality
+
+                    else -> null
+                }
 
                 else -> null
             }
-
-            else -> null
         }
 }
