3b576e82881d6836cf9c95206d30bab71cdbbcc6
Christian Kollmann <christian.kollmann@a-sit.at>
2024-10-29 11:22:34 +0100

    Issuing: Separate view models for selecting host and loading credentials

---
+++ shared/src/commonMain/kotlin/ui/screens/LoadCredentialViewModel.kt
@@ -0,0 +1,16 @@
+package ui.screens
+
+import at.asitplus.jsonpath.core.NormalizedJsonPath
+import at.asitplus.wallet.app.common.WalletMain
+import at.asitplus.wallet.lib.data.ConstantIndex
+
+class LoadCredentialViewModel(
+    val walletMain: WalletMain,
+    val onSubmit: (String, ConstantIndex.CredentialScheme, ConstantIndex.CredentialRepresentation, Set<NormalizedJsonPath>?) -> Unit,
+    val navigateUp: () -> Unit,
+    val hostString: String,
+    val availableSchemes: List<ConstantIndex.CredentialScheme>,
+    val showAttributes: Boolean = true
+) {
+
+}
