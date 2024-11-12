e70e79eceb9932348930c1a7e8c49982eb11bac6
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-11-12 08:02:51 +0100

    cleanup crypto mess

--- shared/src/androidMain/kotlin/at/asitplus/wallet/app/common/AndroidCryptoServiceAuthorizationContext.kt
+++
@@ -1,9 +0,0 @@
-package at.asitplus.wallet.app.common
-
-import android.content.Context
-import androidx.biometric.BiometricPrompt.PromptInfo
-
-data class AndroidCryptoServiceAuthorizationContext(
-    val context: Context,
-    val promptInfo: PromptInfo,
-) : CryptoServiceAuthorizationContext
--- shared/src/iosMain/kotlin/at/asitplus/wallet/app/common/IosCryptoServiceAuthorizationContext.kt
+++
@@ -1,8 +0,0 @@
-package at.asitplus.wallet.app.common
-
-import platform.LocalAuthentication.LAContext
-
-data class IosCryptoServiceAuthorizationContext(
-    val contex: LAContext,
-    val reason: String,
-): CryptoServiceAuthorizationContext
