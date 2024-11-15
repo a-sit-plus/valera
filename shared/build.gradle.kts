6793f334157e1b494360c0ebcdfb508d5427590b
kathrin.resek <kathrin.resek@a-sit.at>
2024-11-15 11:50:13 +0100

    wip add emulator to test yaml

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -5,6 +5,13 @@ import at.asitplus.gradle.serialization
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetTree
 import org.jetbrains.kotlin.gradle.plugin.extraProperties
+import at.asitplus.gradle.ktor
+import at.asitplus.gradle.setupDokka
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClients
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetTree.Companion.test
+import java.net.Socket
+import kotlin.concurrent.thread
 
 plugins {
     kotlin("multiplatform")
@@ -14,22 +21,31 @@ plugins {
     id("at.asitplus.gradle.conventions")
     id("kotlin-parcelize")
     kotlin("plugin.serialization")
+    id("org.jetbrains.dokka")
+    id("maven-publish")
+    id("signing")
 }
 
 kotlin {
     androidTarget {
+        publishLibraryVariants("release")
         @OptIn(ExperimentalKotlinGradlePluginApi::class)
         instrumentedTestVariant {
             sourceSetTree.set(KotlinSourceSetTree.test)
         }
     }
+    jvm()
     iosX64()
     iosArm64()
     iosSimulatorArm64()
 
     sourceSets {
+        all {
+            languageSettings.optIn("kotlin.ExperimentalUnsignedTypes")
+        }
         commonMain {
             dependencies {
+                //api(project(":common"))
                 implementation(compose.runtime)
                 implementation(compose.foundation)
                 implementation(compose.material3)
@@ -55,6 +71,8 @@ kotlin {
                 implementation(ktor("client-logging"))
                 implementation(ktor("client-content-negotiation"))
                 implementation(ktor("serialization-kotlinx-json"))
+                api(ktor("client-encoding"))
+                api(libs.supreme)
             }
         }
 
@@ -122,6 +140,42 @@ android {
         minSdk = (extraProperties["android.minSdk"] as String).toInt()
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+    testBuildType = "debug"
+
+    //just for instrumented tests
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("keystore.p12")
+            storePassword = "123456"
+            keyAlias = "key0"
+            keyPassword = "123456"
+        }
+        create("release") {
+            storeFile = file("keystore.p12")
+            storePassword = "123456"
+            keyAlias = "key0"
+            keyPassword = "123456"
+        }
+    }
+
+    sourceSets.forEach {
+
+        //allow plain traffic and set permissions
+        if (it.name.lowercase().contains("test") || name.lowercase().contains("debug"))
+            it.manifest.srcFile("src/androidInstrumentedTest/AndroidManifest.xml")
+    }
+
+    dependencies {
+        //androidTestImplementation(libs.runner)
+        //androidTestImplementation(libs.core)
+        //androidTestImplementation(libs.rules)
+        //androidTestImplementation(libs.kotest.runner.android)
+        androidTestImplementation(ktor("client-cio"))
+        androidTestImplementation(ktor("serialization-kotlinx-json"))
+        androidTestImplementation(ktor("client-content-negotiation"))
+        //testImplementation(libs.kotest.extensions.android)
+    }
+
 
     packaging {
         resources.excludes += ("META-INF/versions/9/OSGI-INF/MANIFEST.MF")
@@ -143,7 +197,95 @@ android {
     }
 }
 
-repositories {
-    maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-    maven("https://oss.sonatype.org/content/repositories/snapshots/")
+val startVerifier = tasks.register<DefaultTask>("startVerifier") {
+    doLast {
+        if (!kotlin.runCatching { Socket("localhost", 8080) }.fold(onSuccess = { true }, onFailure = { false }))
+            logger.lifecycle("Starting Verifier")
+        else {
+            logger.lifecycle("Shutting down Verifier")
+            runCatching {
+                HttpClients.createDefault().let { client ->
+                    logger.lifecycle("Verifier response: ${client.execute(HttpGet("http://localhost:8080/shutdown")).statusLine.statusCode}")
+                }
+            }.getOrElse { logger.lifecycle("Verifier not running"); it.printStackTrace() }
+
+        }
+        thread(start = true, isDaemon = false) {
+            exec {
+                workingDir = rootDir
+                executable = "./gradlew"
+                args = listOf(":verifier:jvmTest")
+            }
+        }
+
+        logger.lifecycle("Waiting for Verifier to start")
+        while (kotlin.runCatching { Socket("localhost", 8080) }.fold(onSuccess = { true }, onFailure = { false })) {
+            Thread.sleep(1000)
+            logger.lifecycle("Waiting for Verifier to start")
+        }
+        logger.lifecycle("Verifier started")
+    }
 }
+
+val javadocJar = setupDokka(
+    baseUrl = "https://github.com/a-sit-plus/warden-supreme/tree/main/",
+    multiModuleDoc = true
+)
+
+publishing {
+    publications {
+        withType<MavenPublication> {
+            artifact(javadocJar)
+            pom {
+                name.set("WARDEN Supreme Client")
+                description.set("Attestation mobile client; part of the WARDEN Supreme integrated key attestation suite")
+                url.set("https://github.com/a-sit-plus/warden-supreme")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("JesusMcCloud")
+                        name.set("Bernd Prünster")
+                        email.set("bernd.pruenster@a-sit.at")
+                    }
+                    developer {
+                        id.set("nodh")
+                        name.set("Christian Kollmann")
+                        email.set("christian.kollmann@a-sit.at")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git@github.com:a-sit-plus/warden-supreme.git")
+                    developerConnection.set("scm:git:git@github.com:a-sit-plus/warden-supreme.git")
+                    url.set("https://github.com/a-sit-plus/warden-supreme")
+                }
+            }
+        }
+    }
+    repositories {
+        mavenLocal {
+            signing.isRequired = false
+        }
+        maven {
+            url = uri(layout.projectDirectory.dir("..").dir("repo"))
+            name = "local"
+            signing.isRequired = false
+        }
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+}
+
+
+signing {
+    val signingKeyId: String? by project
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+    sign(publishing.publications)
+}
+
