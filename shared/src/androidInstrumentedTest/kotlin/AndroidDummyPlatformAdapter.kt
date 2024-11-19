f479b14063b5074dcc61352ffd4cca2d3ddfd1a7
Michael Dietrich <michael.dietrich@iaik.tugraz.at>
2024-11-19 14:50:25 +0100

    Remove unused override

--- shared/src/androidInstrumentedTest/kotlin/AndroidDummyPlatformAdapter.kt
+++ shared/src/androidInstrumentedTest/kotlin/AndroidDummyPlatformAdapter.kt
@@ -1,19 +1,11 @@
-import android.graphics.BitmapFactory
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asImageBitmap
+
 import at.asitplus.wallet.app.common.PlatformAdapter
 
 class AndroidDummyPlatformAdapter() : PlatformAdapter {
     override fun openUrl(url: String) {
 
     }
-
-    override fun decodeImage(image: ByteArray): ImageBitmap {
-        val bitmap = BitmapFactory.decodeByteArray(image, 0, image.size)
-        bitmap.describeContents()
-        return bitmap.asImageBitmap()
-    }
-
+    
     override fun writeToFile(text: String, fileName: String, folderName: String) {
     }
 
