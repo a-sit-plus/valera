c37e859ae9cf97febf2729fd78ea47160083fbcd
kathrin.resek <kathrin.resek@a-sit.at>
2024-11-22 11:18:29 +0100

    wip run automated tests

--- shared/src/commonTest/kotlin/InstrumentedTests.kt
+++ shared/src/commonTest/kotlin/InstrumentedTests.kt
@@ -268,9 +268,10 @@ class InstrumentedTests {
 
             appLink.value =
             //    "https://wallet.a-sit.at/mobile?request_uri=https://apps.egiz.gv.at/customverifier/transaction/get/af123d37-f736-4f1a-9360-6bb40632987c&client_id=apps.egiz.gv.at&client_metadata_uri=https://apps.egiz.gv.at/customverifier/siopv2/metadata"
-            "https://wallet.a-sit.at/mobile?request_uri=https://apps.egiz.gv.at/customverifier/transaction/get/0ea84341-9ae9-40a0-9376-3432b13c0fb8&client_id=apps.egiz.gv.at&client_metadata_uri=https://apps.egiz.gv.at/customverifier/siopv2/metadata"
-                //waitUntilExactlyOneExists(hasText(getString(Res.string.button_label_consent)), 2000)
-            //onNodeWithText(getString(Res.string.button_label_consent)).performClick()
+         //   "https://wallet.a-sit.at/mobile?request_uri=https://apps.egiz.gv.at/customverifier/transaction/get/0ea84341-9ae9-40a0-9376-3432b13c0fb8&client_id=apps.egiz.gv.at&client_metadata_uri=https://apps.egiz.gv.at/customverifier/siopv2/metadata"
+              "https://wallet.a-sit.at/mobile?request_uri=https://apps.egiz.gv.at/customverifier/transaction/get/de4ffb23-8941-4c70-8b75-aa3b593da13d&client_id=apps.egiz.gv.at&client_metadata_uri=https://apps.egiz.gv.at/customverifier/siopv2/metadata"
+                waitUntilExactlyOneExists(hasText(getString(Res.string.button_label_consent)), 2000)
+            onNodeWithText(getString(Res.string.button_label_consent)).performClick()
         }
     }
 
