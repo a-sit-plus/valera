307a7b6a115323944a2349515b4daf6ddc7d10d2
Michael Dietrich <michael.dietrich@iaik.tugraz.at>
2024-12-19 10:20:19 +0100

    Improve styling of AttributeSelectionGroup

--- shared/src/commonMain/kotlin/ui/composables/credentials/AttributeSelectionGroup.kt
+++ shared/src/commonMain/kotlin/ui/composables/credentials/AttributeSelectionGroup.kt
@@ -1,8 +1,14 @@
 package ui.composables.credentials
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshots.SnapshotStateMap
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import at.asitplus.dif.ConstraintField
 import at.asitplus.jsonpath.core.NodeList
 import at.asitplus.jsonpath.core.NormalizedJsonPath
@@ -18,22 +24,26 @@ fun AttributeSelectionGroup(
     selection: SnapshotStateMap<NormalizedJsonPath, Boolean>,
     format: ConstantIndex.CredentialScheme?
 ) {
-    Column {
-        val constraints = credential.value
-        val disclosedAttributes = constraints.values.mapNotNull { constraint ->
-            constraint.firstOrNull()?.normalizedJsonPath
-        }
-
-        disclosedAttributes.forEach { path ->
-            if (selection[path] == null){
-                selection[path] = true
+    Column(modifier = Modifier.clip(CardDefaults.shape)) {
+        Column(modifier = Modifier
+            .background(color = MaterialTheme.colorScheme.secondaryContainer)
+            .fillMaxWidth()) {
+            val constraints = credential.value
+            val disclosedAttributes = constraints.values.mapNotNull { constraint ->
+                constraint.firstOrNull()?.normalizedJsonPath
             }
-            val stringResource = format?.getLocalization(NormalizedJsonPath(path.segments.last()))
-            if (stringResource != null) {
-                LabeledCheckbox(
-                    label = stringResource(stringResource),
-                    checked = selection[path] ?: true,
-                    onCheckedChange = { bool -> selection[path] = bool })
+
+            disclosedAttributes.forEach { path ->
+                if (selection[path] == null){
+                    selection[path] = true
+                }
+                val stringResource = format?.getLocalization(NormalizedJsonPath(path.segments.last()))
+                if (stringResource != null) {
+                    LabeledCheckbox(
+                        label = stringResource(stringResource),
+                        checked = selection[path] ?: true,
+                        onCheckedChange = { bool -> selection[path] = bool })
+                }
             }
         }
     }
