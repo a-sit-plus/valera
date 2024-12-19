7f615476ab2a6bd15c411262ed1a74a23e81a42b
Michael Dietrich <michael.dietrich@iaik.tugraz.at>
2024-12-19 10:20:19 +0100

    Improve styling of AttributeSelectionGroup

--- shared/src/commonMain/kotlin/ui/composables/credentials/CredentialSelectionCard.kt
+++ shared/src/commonMain/kotlin/ui/composables/credentials/CredentialSelectionCard.kt
@@ -1,13 +1,23 @@
 package ui.composables.credentials
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateMap
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import at.asitplus.dif.ConstraintField
 import at.asitplus.jsonpath.core.NodeList
 import at.asitplus.jsonpath.core.NormalizedJsonPath
@@ -38,7 +48,25 @@ fun CredentialSelectionCard(credential: Map.Entry<SubjectCredentialStore.StoreEn
         )
         CredentialSelectionCardFooter(modifier = Modifier.fillMaxWidth())
     }
-    if (credentialSelection.value == credential.key) {
+
+    val density = LocalDensity.current
+    AnimatedVisibility(
+        visible = selected.value,
+        enter = slideInVertically {
+            with(density) { -20.dp.roundToPx() }
+        } + expandVertically(
+            expandFrom = Alignment.Top
+        ) + fadeIn(
+            initialAlpha = 0.3f
+        ),
+        exit = slideOutVertically {
+            with(density) { 20.dp.roundToPx() }
+        } + shrinkVertically(
+            shrinkTowards = Alignment.Bottom
+        ) + fadeOut(
+            targetAlpha = 0f
+        )
+    ) {
         attributeSelection.clear()
         val format = credential.key.scheme
         AttributeSelectionGroup(credential, format = format, selection = attributeSelection)
