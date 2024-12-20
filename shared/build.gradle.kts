b42982321655ed05c6e9da982e0509e0b3753919
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-12-20 09:54:04 +0100

    fix xcf export mess (#148)

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -20,36 +20,9 @@ plugins {
 
 kotlin {
     androidTarget()
-    val additionalIosExports = listOf(
-        vckCatalog.vck,
-        vckOidCatalog.vck.openid,
-        vckOidCatalog.vck.openid.ktor,
-        libs.credential.ida,
-        libs.credential.mdl,
-        libs.credential.eupid,
-        libs.credential.powerofrepresentation,
-        libs.credential.certificateofresidence,
-        libs.credential.eprescription,
-        kmmresult(),
-        napier()
-    )
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { target ->
-        target.binaries.framework {
-            baseName = "shared"
-            isStatic = false
-            @OptIn(ExperimentalKotlinGradlePluginApi::class)
-            transitiveExport = false
-            embedBitcode(BitcodeEmbeddingMode.DISABLE)
-            additionalIosExports.forEach { export(it) }
-            binaryOption("bundleId", "at.asitplus.wallet.shared")
-            linkerOpts("-ld_classic")
-        }
-    }
-
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
 
     sourceSets {
         commonMain {
