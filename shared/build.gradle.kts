f5fff520d996562c34ba3163766ff261fccdf562
Christian Kollmann <christian.kollmann@a-sit.at>
2025-01-09 09:38:49 +0100

    iOS: Set min. version of framework to 15.0

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -142,6 +142,7 @@ exportXCFramework(
 ) {
     binaryOption("bundleId", "at.asitplus.wallet.shared")
     linkerOpts("-ld_classic")
+    freeCompilerArgs += listOf("-Xoverride-konan-properties=minVersion.ios=15.0;minVersionSinceXcode15.ios=15.0")
 }
 
 repositories {
