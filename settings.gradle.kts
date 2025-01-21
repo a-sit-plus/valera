6f3a5fc5a06d89c3f3adc05d7e8baff7dd3405a0
Michael Dietrich <michael.dietrich@iaik.tugraz.at>
2025-01-21 12:40:47 +0100

    Fix after rebase

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,11 +25,11 @@ plugins {
 }
 
 //If we have a working composite build, use it!
-if (File("./vck/signum").isDirectory && File("./vck/signum/build.gradle.kts").exists()) {
-    logger.warn("Detected VC-K in ${File("./vck").absolutePath}")
+if (File("../vck/signum").isDirectory && File("../vck/signum/build.gradle.kts").exists()) {
+    logger.warn("Detected VC-K in ${File("../vck").absolutePath}.Including it as composite build.")
     logger.warn("Including VC-K and Signum as composite build.")
     logger.warn("If you do not want this, move the VC-K to another location!")
-    includeBuild("./vck/signum") {
+    includeBuild("../vck/signum") {
         dependencySubstitution {
             substitute(module("at.asitplus.wallet:indispensable")).using(project(":indispensable"))
             substitute(module("at.asitplus.signum:indispensable-josef")).using(project(":indispensable-josef"))
@@ -37,7 +37,7 @@ if (File("./vck/signum").isDirectory && File("./vck/signum/build.gradle.kts").ex
             substitute(module("at.asitplus.signum:supreme")).using(project(":supreme"))
         }
     }
-    includeBuild("./vck") {
+    includeBuild("../vck") {
         dependencySubstitution {
             substitute(module("at.asitplus.wallet:vck")).using(project(":vck"))
             substitute(module("at.asitplus.wallet:vck-openid")).using(project(":vck-openid"))
@@ -45,6 +45,7 @@ if (File("./vck/signum").isDirectory && File("./vck/signum/build.gradle.kts").ex
             substitute(module("at.asitplus.wallet:vck-openid-ktor")).using(project(":vck-openid-ktor"))
             substitute(module("at.asitplus.wallet:openid-data-classes")).using(project(":openid-data-classes"))
             substitute(module("at.asitplus.wallet:dif-data-classes")).using(project(":dif-data-classes"))
+            substitute(module("at.asitplus.wallet:vck-rqes")).using(project(":vck-rqes"))
             substitute(module("at.asitplus.wallet:rqes-data-classes")).using(project(":rqes-data-classes"))
         }
     }
