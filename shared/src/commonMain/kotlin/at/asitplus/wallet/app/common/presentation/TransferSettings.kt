b9ef44773240e6b9e68ef4fab670edf9a0debd5a
Gerald Palfinger <gerald.palfinger@a-sit.at>
2025-02-18 18:04:49 +0100

    Enable basic Bluetooth support
    Permission needs to be granted manually from the settings screen

--- shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/presentation/TransferSettings.kt
+++ shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/presentation/TransferSettings.kt
@@ -2,13 +2,13 @@ package at.asitplus.wallet.app.common.presentation
 
 data class TransferSettings(
     val presentmentNegotiatedHandoverPreferredOrder: List<String> = listOf(
-        //"ble:central_client_mode:",
-        //"ble:peripheral_server_mode:",
+        "ble:central_client_mode:",
+        "ble:peripheral_server_mode:",
         "nfc:"
     ),
     val presentmentUseNegotiatedHandover: Boolean = true,
-    val presentmentBleCentralClientModeEnabled: Boolean = false,
-    val presentmentBlePeripheralServerModeEnabled: Boolean = false,
+    val presentmentBleCentralClientModeEnabled: Boolean = true,
+    val presentmentBlePeripheralServerModeEnabled: Boolean = true,
     val presentmentNfcDataTransferEnabled: Boolean = true,
     val readerBleL2CapEnabled: Boolean = false,
     val presentmentAllowMultipleRequests: Boolean = false,
