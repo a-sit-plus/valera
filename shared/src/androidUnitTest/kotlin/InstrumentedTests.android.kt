345f793be6ad4ac6990c90c8ff65c07640361905
Gerald Palfinger <gerald.palfinger@a-sit.at>
2025-03-13 16:37:13 +0100

    Maybe fix tests

--- shared/src/androidUnitTest/kotlin/InstrumentedTests.android.kt
+++ shared/src/androidUnitTest/kotlin/InstrumentedTests.android.kt
@@ -1,9 +1,11 @@
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import at.asitplus.wallet.app.common.PlatformAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 
 @Composable
 actual fun getPlatformAdapter(): PlatformAdapter {
     val context = LocalContext.current
-    return AndroidPlatformAdapter(context, {}, localNfcReader, scope)
+    return AndroidPlatformAdapter(context, {}, CoroutineScope(Dispatchers.Default))
 }
