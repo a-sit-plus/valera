cbd669b1f52d7a0c9a982d4d9943659f19dba437
Gerald Palfinger <gerald.palfinger@a-sit.at>
2025-03-14 13:19:21 +0100

    Use catching syntax

--- shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/domain/BuildAuthenticationConsentPageFromAuthenticationRequestLocalPresentment.kt
+++ shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/domain/BuildAuthenticationConsentPageFromAuthenticationRequestLocalPresentment.kt
@@ -1,20 +1,19 @@
 package at.asitplus.wallet.app.common.domain
 
 import at.asitplus.KmmResult
+import at.asitplus.catching
+import at.asitplus.catchingUnwrapped
 import at.asitplus.wallet.app.common.presentation.PresentationRequest
 import presentationStateModel
 import ui.navigation.routes.LocalPresentationAuthenticationConsentRoute
 
 class BuildAuthenticationConsentPageFromAuthenticationRequestLocalPresentment {
-    operator fun invoke(incomingRequest: PresentationRequest?): KmmResult<LocalPresentationAuthenticationConsentRoute> {
-        if (presentationStateModel.value == null)
-            return KmmResult.failure(Error("No presentationStateModel set"))
-        return incomingRequest?.let {
-            KmmResult.success(
-                LocalPresentationAuthenticationConsentRoute(
-                    it.serialize()
-                )
+    operator fun invoke(incomingRequest: PresentationRequest?): KmmResult<LocalPresentationAuthenticationConsentRoute> =
+        catching {
+            require(presentationStateModel.value != null) { "No presentationStateModel set" }
+            require(incomingRequest != null) { "No presentation request received" }
+            LocalPresentationAuthenticationConsentRoute(
+                incomingRequest.serialize()
             )
-        } ?: KmmResult.failure(Error("No presentation request received"))
-    }
+        }
 }
