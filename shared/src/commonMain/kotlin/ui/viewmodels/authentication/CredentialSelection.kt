ac0d1db61da5d0a9d3cacafac580c8a2af01962c
acrusage <139230478+acrusage-iaik@users.noreply.github.com>
2025-03-21 17:18:34 +0100

    Add: Default DCQL presentation

--- shared/src/commonMain/kotlin/ui/viewmodels/authentication/CredentialSelection.kt
+++ shared/src/commonMain/kotlin/ui/viewmodels/authentication/CredentialSelection.kt
@@ -4,10 +4,6 @@ import at.asitplus.openid.dcql.DCQLCredentialQueryIdentifier
 import at.asitplus.openid.dcql.DCQLCredentialSubmissionOption
 import at.asitplus.wallet.lib.agent.PresentationExchangeCredentialDisclosure
 import at.asitplus.wallet.lib.agent.SubjectCredentialStore
-import at.asitplus.wallet.lib.data.CredentialPresentation
-import at.asitplus.wallet.lib.data.CredentialPresentationRequest
-import kotlinx.serialization.Serializable
-import ui.viewmodels.CredentialSelection
 
 sealed interface CredentialPresentationSubmissions<Credential: Any>
 
