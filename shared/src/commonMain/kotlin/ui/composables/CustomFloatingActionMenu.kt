8c27d75bb688beadafbdce9546824732a895fa31
Michael <144923189+md4096@users.noreply.github.com>
2025-04-16 13:28:37 +0200

    Change color and border

--- shared/src/commonMain/kotlin/ui/composables/CustomFloatingActionMenu.kt
+++ shared/src/commonMain/kotlin/ui/composables/CustomFloatingActionMenu.kt
@@ -18,19 +18,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.QrCode
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -48,7 +49,12 @@ import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun CustomFloatingActionMenu(addCredentialQr: () -> Unit, addCredential: () -> Unit) {
-    val expanded = remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
+
+    val onChangeExpanded: (Boolean) -> Unit = { bool ->
+        expanded = bool
+    }
+
     Box(modifier = Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -58,7 +64,7 @@ fun CustomFloatingActionMenu(addCredentialQr: () -> Unit, addCredential: () -> U
             Column(horizontalAlignment = Alignment.End) {
                 val density = LocalDensity.current
                 AnimatedVisibility(
-                    visible = expanded.value,
+                    visible = expanded,
                     enter = slideInVertically {
                         with(density) { -20.dp.roundToPx() }
                     } + expandVertically(
@@ -89,24 +95,34 @@ fun CustomFloatingActionMenu(addCredentialQr: () -> Unit, addCredential: () -> U
                     }
                 }
                 Spacer(modifier = Modifier.size(10.dp))
-                MainFloatingActionButton(expanded = expanded)
+                MainFloatingActionButton(expanded = expanded, onChangeExpanded = onChangeExpanded)
             }
         }
     }
 }
 
 @Composable
-fun MainFloatingActionButton(expanded: MutableState<Boolean>) {
+fun MainFloatingActionButton(expanded: Boolean, onChangeExpanded: (Boolean) -> Unit) {
+    val colors = when (expanded) {
+        true -> ButtonDefaults.outlinedButtonColors()
+        false -> ButtonDefaults.buttonColors()
+    }
+    val border = when (expanded) {
+        true -> ButtonDefaults.outlinedButtonBorder()
+        false -> null
+    }
     Button(
-        onClick = { expanded.value = !expanded.value },
+        onClick = { onChangeExpanded(!expanded) },
         modifier = Modifier.size(50.dp),
+        colors = colors,
+        border = border,
         shape = RoundedCornerShape(15.dp),
         contentPadding = PaddingValues(0.dp),
     ) {
         Icon(
             imageVector = Icons.Default.Add,
             contentDescription = null,
-            modifier = Modifier.rotate(if (expanded.value) 45f else 0f)
+            modifier = Modifier.rotate(if (expanded) 45f else 0f)
         )
     }
 }
