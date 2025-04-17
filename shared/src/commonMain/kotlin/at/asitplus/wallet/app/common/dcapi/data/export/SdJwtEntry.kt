c3e94ee26f5ae4e6dc8a8b52dd9874497f426b8b
Gerald Palfinger <gerald.palfinger@a-sit.at>
2025-04-17 14:37:08 +0200

    Add SD-JWT export support

--- shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/dcapi/data/export/SdJwtEntry.kt
+++ shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/dcapi/data/export/SdJwtEntry.kt
@@ -3,15 +3,22 @@ package at.asitplus.wallet.app.common.dcapi.data.export
 import at.asitplus.KmmResult
 import at.asitplus.catching
 import at.asitplus.signum.indispensable.cosef.io.coseCompliantSerializer
+import data.credentials.CredentialAttributeTranslator
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromByteArray
 import kotlinx.serialization.encodeToByteArray
+import kotlinx.serialization.json.JsonPrimitive
+import org.jetbrains.compose.resources.getString
 
 @Serializable
 data class SdJwtEntry(
-    @SerialName("todo")
-    val todo: String
+    @SerialName("id")
+    val jwtId: String,
+    @SerialName("vct")
+    val verifiableCredentialType: String,
+    @SerialName("claims")
+    val claims: Map<String, ExportedElements>
 ) {
     fun serialize(): ByteArray = coseCompliantSerializer.encodeToByteArray(this)
 
@@ -19,5 +26,14 @@ data class SdJwtEntry(
         fun deserialize(it: ByteArray): KmmResult<SdJwtEntry> = catching {
             coseCompliantSerializer.decodeFromByteArray(it)
         }
+
+        suspend fun fromAttributeMap(
+            attributeMap: Map<String, JsonPrimitive>,
+            attributeTranslator: CredentialAttributeTranslator
+        ): Map<String, ExportedElements> = attributeMap.map { (name, value) ->
+            val displayName = attributeTranslator.translate(name.toJsonPath())?.let { getString(it) } ?: name
+            val previewValue = value.toCustomString().safeSubstring(128)
+            name to ExportedElements(displayName, previewValue)
+        }.toMap()
     }
 }
