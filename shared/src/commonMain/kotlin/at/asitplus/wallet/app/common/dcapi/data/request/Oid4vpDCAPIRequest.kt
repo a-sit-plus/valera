19726c3127058c400a21f9a05a43f2361e2eb908
Gerald Palfinger <gerald.palfinger@a-sit.at>
2025-05-03 18:29:37 +0200

    WIP: OID4VP over DCAPI

--- shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/dcapi/data/request/Oid4vpDCAPIRequest.kt
+++ shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/dcapi/data/request/Oid4vpDCAPIRequest.kt
@@ -1,12 +1,21 @@
-package at.asitplus.wallet.app.common.dcapi.data.oid4vp
+package at.asitplus.wallet.app.common.dcapi.data.request
 
 import at.asitplus.catching
-import at.asitplus.wallet.app.common.dcapi.data.DCAPIRequest
 import at.asitplus.wallet.lib.data.vckJsonSerializer
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Oid4vpDCAPIRequest(val protocol: String, val request: String) : DCAPIRequest() {
+data class Oid4vpDCAPIRequest(
+    val protocol: String,
+    val request: String,
+    val credentialId: Int,
+    val callingPackageName: String?,
+    val callingOrigin: String?
+) : DCAPIRequest() {
+    init {
+        require(callingOrigin != null || callingPackageName != null)
+    }
+
     override fun serialize(): String = vckJsonSerializer.encodeToString(this)
 
     companion object {
