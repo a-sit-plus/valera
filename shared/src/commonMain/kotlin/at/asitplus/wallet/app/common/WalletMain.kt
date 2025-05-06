2eac6fdf3ffc27ba36477e47a26700d0ec748436
Gerald Palfinger <gerald.palfinger@a-sit.at>
2025-05-06 11:28:24 +0200

    Basic oid4vp workflow done

--- shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/WalletMain.kt
+++ shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/WalletMain.kt
@@ -316,6 +316,7 @@ interface PlatformAdapter {
      * Prepares the credential response and sends it back to the invoking application
      */
     fun prepareDCAPICredentialResponse(responseJson: ByteArray, dcApiRequestPreview: PreviewDCAPIRequest)
+    fun prepareDCAPICredentialResponse(responseJson: String)
 
 }
 
@@ -347,6 +348,9 @@ class DummyPlatformAdapter : PlatformAdapter {
         responseJson: ByteArray,
         dcApiRequestPreview: PreviewDCAPIRequest
     ) {
+    }
+
+    override fun prepareDCAPICredentialResponse(responseJson: String) {
     }
 
 }
