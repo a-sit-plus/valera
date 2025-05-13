178d3c078974d3821eb92df5219c737d8b88e5f9
Kathrin <155988774+kathrin-7978@users.noreply.github.com>
2025-05-13 16:09:50 +0200

    Fix/ios UI tests (#265)
    * add composite
    
    * fix kotlin.IllegalStateException on ios: CompositionLocal LocalLifecycleOwner not present
    
    * remove waiting for loadingTestTag
    
    * Add submodule
    
    * Use submodule for composite build
    
    * Use new branch
    
    * Add POST_NOTIFICATIONS permission
    
    * add platform specific httpclient and allow self signed certificates for ios tests
    
    * fix path test report
    
    * refactoring
    
    * Update AndroidManifest.xml
    
    * Start iOS Sim
    
    * Use release version of identity-credential
    
    * Revert "add platform specific httpclient and allow self signed certificates for ios tests"
    
    This reverts commit f7a28143799239bdd63c895b63d0a06b2684679a.
    
    ---------
    
    Co-authored-by: Michael <144923189+md4096@users.noreply.github.com>

--- shared/src/commonTest/kotlin/InstrumentedTestsSuite.kt
+++ shared/src/commonTest/kotlin/InstrumentedTestsSuite.kt
@@ -130,14 +130,11 @@ class InstrumentedTestsSuite : FunSpec({
         test("App should start correctly") {
             runComposeUiTest {
                 setContent {
-                    val platformAdapter = getPlatformAdapter()
-                    val walletMain = createWalletMain(platformAdapter)
-                    App(walletMain)
-                }
-
-                waitUntil {
-                    onNodeWithTag(NavigatorTestTags.loadingTestTag)
-                        .isNotDisplayed()
+                    CompositionLocalProvider(
+                        LocalLifecycleOwner provides TestLifecycleOwner()
+                    ) {
+                        App(createWalletMain(getPlatformAdapter()))
+                    }
                 }
 
                 waitUntil {
@@ -153,14 +150,11 @@ class InstrumentedTestsSuite : FunSpec({
         test("Test 2: App should display onboarding screen") {
             runComposeUiTest {
                 setContent {
-                    val platformAdapter = getPlatformAdapter()
-                    val walletMain = createWalletMain(platformAdapter)
-                    App(walletMain)
-                }
-
-                waitUntil {
-                    onNodeWithTag(NavigatorTestTags.loadingTestTag)
-                        .isNotDisplayed()
+                    CompositionLocalProvider(
+                        LocalLifecycleOwner provides TestLifecycleOwner()
+                    ) {
+                        App(createWalletMain(getPlatformAdapter()))
+                    }
                 }
 
                 onNodeWithTag(OnboardingWrapperTestTags.onboardingStartScreen)
@@ -172,14 +166,11 @@ class InstrumentedTestsSuite : FunSpec({
         test("Test 3: App should show onboarding start button") {
             runComposeUiTest {
                 setContent {
-                    val platformAdapter = getPlatformAdapter()
-                    val walletMain = createWalletMain(platformAdapter)
-                    App(walletMain)
-                }
-
-                waitUntil {
-                    onNodeWithTag(NavigatorTestTags.loadingTestTag)
-                        .isNotDisplayed()
+                    CompositionLocalProvider(
+                        LocalLifecycleOwner provides TestLifecycleOwner()
+                    ) {
+                        App(createWalletMain(getPlatformAdapter()))
+                    }
                 }
 
                 waitUntil {
@@ -199,10 +190,13 @@ class InstrumentedTestsSuite : FunSpec({
             runComposeUiTest {
                 lateinit var walletMain: WalletMain
                 setContent {
-                    CompositionLocalProvider(LocalLifecycleOwner provides lifecycleOwner) {
+                    CompositionLocalProvider(
+                        LocalLifecycleOwner provides TestLifecycleOwner()
+                    ) {
                         val platformAdapter = getPlatformAdapter()
                         walletMain = createWalletMain(platformAdapter)
                         App(walletMain)
+                    }
 
                         val keyMaterial = EphemeralKeyWithoutCert()
                         val issuer = IssuerAgent(
@@ -224,7 +218,6 @@ class InstrumentedTestsSuite : FunSpec({
                                 ).getOrThrow().toStoreCredentialInput()
                             )
                         }
-                    }
                 }
                 runBlocking {
                     waitUntilExactlyOneExists(hasText(getString(Res.string.button_label_start)))
