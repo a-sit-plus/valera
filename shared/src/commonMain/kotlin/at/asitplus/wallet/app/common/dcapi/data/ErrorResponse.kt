e95ce64e1f9b7252e0aeff592d7539f31982a984
Gerald Palfinger <gerald.palfinger@a-sit.at>
2025-05-26 13:09:34 +0200

    Fix serializing error response

--- shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/dcapi/data/ErrorResponse.kt
+++ shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/dcapi/data/ErrorResponse.kt
@@ -1,7 +1,9 @@
 package at.asitplus.wallet.app.common.dcapi.data
 
 import at.asitplus.wallet.lib.data.vckJsonSerializer
+import kotlinx.serialization.Serializable
 
+@Serializable
 class ErrorResponse(val error: String) {
     fun serialize() = vckJsonSerializer.encodeToString(this)
 }
