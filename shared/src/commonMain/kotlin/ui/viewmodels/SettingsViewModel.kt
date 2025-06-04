954ad612b6558a479b229ba9d2fc2ec89be898ae
acrusage <139230478+acrusage-iaik@users.noreply.github.com>
2025-06-04 19:35:20 +0200

    Change: Use Koin ViewModel

--- shared/src/commonMain/kotlin/ui/viewmodels/SettingsViewModel.kt
+++ shared/src/commonMain/kotlin/ui/viewmodels/SettingsViewModel.kt
@@ -1,7 +1,8 @@
 package ui.viewmodels
 
+import androidx.lifecycle.ViewModel
 import at.asitplus.wallet.app.common.data.SettingsRepository
 
 class SettingsViewModel(
     private val settingsRepository: SettingsRepository,
-) : SettingsRepository by settingsRepository
+) : SettingsRepository by settingsRepository, ViewModel()
