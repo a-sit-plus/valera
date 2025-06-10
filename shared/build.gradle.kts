bc90f4fb98c61a5d6b3779db28a552ccc337932c
Michael <144923189+md4096@users.noreply.github.com>
2025-06-10 11:41:51 +0200

    Add publishLibraryVariants

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -193,3 +193,9 @@ repositories {
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
 }
+
+kotlin {
+    androidTarget {
+        publishLibraryVariants("release")
+    }
+}
