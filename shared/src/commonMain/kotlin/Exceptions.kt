f5765373f3f356bec3fc81bf369cc84aad137a46
Michael <144923189+md4096@users.noreply.github.com>
2025-06-16 17:12:12 +0200

    Update shared/src/commonMain/kotlin/Exceptions.kt
    Co-authored-by: Christian Kollmann <christian.kollmann@a-sit.at>

--- shared/src/commonMain/kotlin/Exceptions.kt
+++ shared/src/commonMain/kotlin/Exceptions.kt
@@ -1,5 +1,5 @@
 class UncorrectableErrorException(override val cause: Throwable?) : Throwable(cause = cause)
 
-object AppResetRequiredException : Throwable(message = "AppResetRequiredException") {
+object AppResetRequiredException : Throwable("App needs reset due to major changes.") {
     override fun toString() = "AppResetRequiredException"
 }
