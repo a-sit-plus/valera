6d352725fca45b318dd899fb569873cfcdfbbb0d
acrusage <139230478+acrusage-iaik@users.noreply.github.com>
2025-07-02 16:41:28 +0200

    Add: Separate Credential Attribute Categorization

---
+++ shared/src/commonMain/kotlin/data/credentials/CompanyRegistrationCredentialClaimDefinition.kt
@@ -0,0 +1,17 @@
+package data.credentials
+
+enum class CompanyRegistrationCredentialClaimDefinition {
+    COMPANY_NAME,
+    COMPANY_TYPE,
+    COMPANY_STATUS,
+    COMPANY_ACTIVITY,
+    REGISTRATION_DATE,
+    COMPANY_END_DATE,
+    COMPANY_EUID,
+    VAT_NUMBER,
+    COMPANY_CONTACT_DATA,
+    REGISTERED_ADDRESS,
+    POSTAL_ADDRESS,
+    BRANCH,
+    ;
+}
