ae7ab633e4976c0441ab08055f6d537146661d9b
Michael <144923189+md4096@users.noreply.github.com>
2025-07-18 16:55:08 +0200

    Add new sonatype maven-snapshots repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -57,6 +57,7 @@ dependencyResolutionManagement {
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+        maven("https://central.sonatype.com/repository/maven-snapshots/")
     }
 
     versionCatalogs {
