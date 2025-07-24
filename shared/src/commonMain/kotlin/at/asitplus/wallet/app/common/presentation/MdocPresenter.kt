29a8289875f1049b343703d69cc2c20af6624d12
Christian Kollmann <christian.kollmann@a-sit.at>
2025-07-24 09:37:13 +0200

    Proximity: Refactor presentation

--- shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/presentation/MdocPresenter.kt
+++ shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/presentation/MdocPresenter.kt
@@ -1,7 +1,6 @@
 package at.asitplus.wallet.app.common.presentation
 
 import at.asitplus.dcapi.NFCHandover
-import at.asitplus.iso.DeviceRequest
 import at.asitplus.iso.SessionTranscript
 import at.asitplus.signum.indispensable.cosef.CoseKey
 import at.asitplus.signum.indispensable.cosef.io.coseCompliantSerializer
@@ -12,10 +11,12 @@ import kotlinx.serialization.decodeFromByteArray
 import kotlinx.serialization.encodeToByteArray
 import org.multipaz.cbor.Cbor
 import org.multipaz.cbor.Simple
+import org.multipaz.crypto.EcPublicKey
 import org.multipaz.mdoc.request.DeviceRequestParser
 import org.multipaz.mdoc.role.MdocRole
 import org.multipaz.mdoc.sessionencryption.SessionEncryption
 import org.multipaz.mdoc.transport.MdocTransport
+import org.multipaz.mdoc.transport.MdocTransport.State
 import org.multipaz.mdoc.transport.MdocTransportClosedException
 import org.multipaz.util.Constants
 import ui.viewmodels.authentication.PresentationStateModel
@@ -37,111 +38,15 @@ class MdocPresenter(
         val transport = mechanism.transport
         // Wait until state changes to CONNECTED, FAILED, or CLOSED
         transport.state.first {
-            it == MdocTransport.State.CONNECTED ||
-                    it == MdocTransport.State.FAILED ||
-                    it == MdocTransport.State.CLOSED
+            it in listOf(State.CONNECTED, State.FAILED, State.CLOSED)
         }
-        if (transport.state.value != MdocTransport.State.CONNECTED) {
+        if (transport.state.value != State.CONNECTED) {
             stateModel.setCompleted(Error("Expected state CONNECTED but found ${transport.state.value}"))
             return
         }
 
         try {
-            var sessionEncryption: SessionEncryption? = null
-            var encodedSessionTranscript: ByteArray? = null
-            var sessionTranscript: SessionTranscript? = null
-            while (true) {
-                Napier.i("Waiting for message from reader...")
-                dismissible.value = true
-                val sessionData = transport.waitForMessage()
-                dismissible.value = false
-                if (sessionData.isEmpty()) {
-                    Napier.i("Received transport-specific session termination message from reader")
-                    stateModel.setCompleted()
-                    break
-                }
-
-                if (sessionEncryption == null) {
-                    val eReaderKey = SessionEncryption.getEReaderKey(sessionData)
-                    val eReaderCoseKey =
-                        CoseKey.deserialize(Cbor.encode(eReaderKey.toCoseKey().toDataItem()))
-
-                    sessionTranscript = if (mechanism.handover == Simple.NULL) {
-                        SessionTranscript.forQr(
-                            deviceEngagementBytes = mechanism.encodedDeviceEngagement.toByteArray(),
-                            eReaderKeyBytes = eReaderCoseKey.getOrThrow().serialize()
-                        )
-                    } else {
-                        val nfcHandover = coseCompliantSerializer.decodeFromByteArray<NFCHandover>(
-                            Cbor.encode(mechanism.handover)
-                        )
-                        SessionTranscript.forNfc(
-                            deviceEngagementBytes = mechanism.encodedDeviceEngagement.toByteArray(),
-                            eReaderKeyBytes = eReaderCoseKey.getOrThrow().serialize(),
-                            nfcHandover = nfcHandover
-                        )
-                    }
-
-                    encodedSessionTranscript =
-                        coseCompliantSerializer.encodeToByteArray(sessionTranscript)
-
-                    sessionEncryption = SessionEncryption(
-                        MdocRole.MDOC,
-                        mechanism.ephemeralDeviceKey,
-                        eReaderKey,
-                        encodedSessionTranscript,
-                    )
-                }
-                val (encodedDeviceRequest, status) = sessionEncryption.decryptMessage(sessionData)
-
-                if (status == Constants.SESSION_DATA_STATUS_SESSION_TERMINATION) {
-                    Napier.i("mdocPresentment: Received session termination message from reader")
-                    stateModel.setCompleted()
-                    break
-                }
-
-                //TODO use our libs to check the reader authentication
-                DeviceRequestParser(
-                    encodedDeviceRequest!!,
-                    encodedSessionTranscript!!,
-                ).parse()
-
-                val deviceRequest =
-                    coseCompliantSerializer.decodeFromByteArray<DeviceRequest>(encodedDeviceRequest)
-
-                presentationViewModel.initWithDeviceRequest(
-                    deviceRequest,
-                    credentialSelected,
-                    sessionTranscript
-                )
-
-                val response = stateModel.requestCredentialSelection()
-
-                mechanism.transport.sendMessage(
-                    sessionEncryption.encryptMessage(
-                        response,
-                        if (!mechanism.allowMultipleRequests) {
-                            Constants.SESSION_DATA_STATUS_SESSION_TERMINATION
-                        } else {
-                            null
-                        }
-                    )
-                )
-
-                numRequestsServed.value += 1
-                if (!mechanism.allowMultipleRequests) {
-                    // Wait for transport to be closed as sendMessage does not block as advertised in its description
-                    transport.state.first {
-                        it == MdocTransport.State.CLOSED ||
-                                it == MdocTransport.State.FAILED
-                    }
-                    Napier.i("Response sent, closing connection")
-                    stateModel.setCompleted()
-                    break
-                } else {
-                    Napier.i("Response sent, keeping connection open")
-                }
-            }
+            runProcess(dismissible, numRequestsServed, credentialSelected, transport)
         } catch (_: MdocTransportClosedException) {
             // Nothing to do, this is thrown when transport.close() is called from another coroutine, that
             // is, the X in the top-right
@@ -153,4 +58,100 @@ class MdocPresenter(
         }
         transport.close()
     }
+
+    private suspend fun runProcess(
+        dismissible: MutableStateFlow<Boolean>,
+        numRequestsServed: MutableStateFlow<Int>,
+        credentialSelected: (ByteArray) -> Unit,
+        transport: MdocTransport
+    ) {
+        var sessionEncryption: SessionEncryption? = null
+        var encodedSessionTranscript: ByteArray? = null
+        var sessionTranscript: SessionTranscript? = null
+        while (true) {
+            Napier.i("Waiting for message from reader...")
+            dismissible.value = true
+            val sessionData = transport.waitForMessage()
+            dismissible.value = false
+            if (sessionData.isEmpty()) {
+                Napier.i("Received transport-specific session termination message from reader")
+                stateModel.setCompleted()
+                break
+            }
+
+            if (sessionEncryption == null) {
+                val eReaderKey = SessionEncryption.getEReaderKey(sessionData)
+                sessionTranscript = calcSessionTranscript(eReaderKey)
+                encodedSessionTranscript = coseCompliantSerializer.encodeToByteArray(sessionTranscript)
+                sessionEncryption = SessionEncryption(
+                    MdocRole.MDOC,
+                    mechanism.ephemeralDeviceKey,
+                    eReaderKey,
+                    encodedSessionTranscript,
+                )
+            }
+            val (encodedDeviceRequest, status) = sessionEncryption.decryptMessage(sessionData)
+
+            if (status == Constants.SESSION_DATA_STATUS_SESSION_TERMINATION) {
+                Napier.i("mdocPresentment: Received session termination message from reader")
+                stateModel.setCompleted()
+                break
+            }
+
+            //TODO use our libs to check the reader authentication
+            DeviceRequestParser(
+                encodedDeviceRequest = encodedDeviceRequest!!,
+                encodedSessionTranscript = encodedSessionTranscript!!,
+            ).parse()
+
+            presentationViewModel.initWithDeviceRequest(
+                parsedRequest = coseCompliantSerializer.decodeFromByteArray(encodedDeviceRequest),
+                finishFunction = credentialSelected,
+                sessionTranscript = sessionTranscript
+            )
+            val response = stateModel.requestCredentialSelection()
+
+            mechanism.transport.sendMessage(response.encrypt(sessionEncryption))
+
+            numRequestsServed.value += 1
+            if (!mechanism.allowMultipleRequests) {
+                // Wait for transport to be closed as sendMessage does not block as advertised in its description
+                transport.state.first {
+                    it in listOf(State.CLOSED, State.FAILED)
+                }
+                Napier.i("Response sent, closing connection")
+                stateModel.setCompleted()
+                break
+            } else {
+                Napier.i("Response sent, keeping connection open")
+            }
+        }
+    }
+
+    private fun ByteArray.encrypt(
+        sessionEncryption: SessionEncryption
+    ): ByteArray = sessionEncryption.encryptMessage(
+        messagePlaintext = this,
+        statusCode = if (!mechanism.allowMultipleRequests) Constants.SESSION_DATA_STATUS_SESSION_TERMINATION else null
+    )
+
+    private fun calcSessionTranscript(eReaderKey: EcPublicKey): SessionTranscript =
+        if (mechanism.handover == Simple.NULL) {
+            SessionTranscript.forQr(
+                deviceEngagementBytes = mechanism.encodedDeviceEngagement.toByteArray(),
+                eReaderKeyBytes = eReaderKey.getBytes()
+            )
+        } else {
+            val nfcHandover = coseCompliantSerializer.decodeFromByteArray<NFCHandover>(
+                Cbor.encode(mechanism.handover)
+            )
+            SessionTranscript.forNfc(
+                deviceEngagementBytes = mechanism.encodedDeviceEngagement.toByteArray(),
+                eReaderKeyBytes = eReaderKey.getBytes(),
+                nfcHandover = nfcHandover
+            )
+        }
+
+    private fun EcPublicKey.getBytes(): ByteArray =
+        CoseKey.deserialize(Cbor.encode(toCoseKey().toDataItem())).getOrThrow().serialize()
 }
