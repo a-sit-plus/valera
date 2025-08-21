7a07d0d0d8d162c991d5aa81f13240a1168592da
Lukas Posch <lukas.posch@a-sit.at>
2025-08-21 16:52:19 +0200

    Add views for transfer method infos

---
+++ shared/src/commonMain/kotlin/ui/views/iso/common/CenteredInfoText.kt
@@ -0,0 +1,31 @@
+package ui.views.iso.common
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CenteredInfoText(
+    message: String,
+    modifier: Modifier = Modifier,
+    buttons: @Composable ColumnScope.() -> Unit = {}
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(message, textAlign = TextAlign.Center)
+        Spacer(Modifier.height(8.dp))
+        buttons()
+    }
+}
