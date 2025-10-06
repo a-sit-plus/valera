4157d7488a53d9ecb3aa9704a3c71334d610bdfa
Michael <144923189+md4096@users.noreply.github.com>
2025-10-06 12:39:22 +0200

    Reimplement SwiftPlatformAdapter in Kotlin

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -11,7 +11,6 @@ struct ComposeView: UIViewControllerRepresentable {
         let buildType = BuildType.release_
         #endif
         return Main_iosKt.MainViewController(
-            platformAdapter: SwiftPlatformAdapter(),
             buildContext: BuildContext(
                 buildType: buildType,
                 packageName: Bundle.main.bundleIdentifier ?? "at.asitplus.wallet.compose",
@@ -33,116 +32,5 @@ struct ContentView: View {
                     Globals.shared.appLink.setValue(url.absoluteString)
                 }
     }
-}
-
-class SwiftPlatformAdapter: PlatformAdapter {
-
-    func openUrl(url: String) {
-        DispatchQueue.main.async {
-            if let uri = URL(string: url) {
-                UIApplication.shared.open(uri)
-            }
-        }
-    }
-
-    func writeToFile(text: String, fileName: String, folderName: String) {
-        if let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-            let folderUrl = url.appendingPathComponent(folderName)
-            if !FileManager.default.fileExists(atPath: folderUrl.path){
-                createDirectory(folderName: folderName)
-            }
-            let fileUrl = folderUrl.appendingPathComponent(fileName)
-            if let file = try? FileHandle(forWritingTo: fileUrl) {
-                file.seekToEndOfFile()
-                file.write(text.data(using: .utf8)!)
-                file.closeFile()
-            } else {
-                FileManager.default.createFile(atPath: fileUrl.path, contents: text.data(using: .utf8))
-            }
-        }
-    }
-    func readFromFile(fileName: String, folderName: String) -> String? {
-        if let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-            let folderUrl = url.appendingPathComponent(folderName)
-            if !FileManager.default.fileExists(atPath: folderUrl.path){
-                createDirectory(folderName: folderName)
-            }
-            let fileUrl = folderUrl.appendingPathComponent(fileName)
-            do {
-                let log = try String(contentsOf: fileUrl, encoding: .utf8)
-                return log
-            } catch {
-                NapierProxy.companion.d(msg: "Unable to read from file: \(fileName)")
-                return nil
-            }
-        }
-        return nil
-    }
-
-    func clearFile(fileName: String, folderName: String) {
-        if let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-            let folderUrl = url.appendingPathComponent(folderName)
-            if !FileManager.default.fileExists(atPath: folderUrl.path){
-                createDirectory(folderName: folderName)
-            }
-            let fileUrl = folderUrl.appendingPathComponent(fileName)
-            do {
-                try FileManager().removeItem(at: fileUrl)
-            } catch {
-                NapierProxy.companion.d(msg: "Unable to clear file: \(fileName)")
-            }
-        }
-    }
-
-    func createDirectory(folderName: String) {
-        if let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-            let folderUrl = url.appendingPathComponent(folderName)
-            do {
-                try FileManager.default.createDirectory(at: folderUrl, withIntermediateDirectories: true)
-            } catch {
-                NapierProxy.companion.d(msg: "Unable to create from directory: \(folderUrl)")
-            }
-        }
-    }
-
-    func shareLog() {
-        if let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-            let folderUrl = url.appendingPathComponent("logs")
-            let fileUrl = folderUrl.appendingPathComponent("log.txt") as NSURL
-            
-            DispatchQueue.main.async {
-                let connectedScenes = UIApplication.shared.connectedScenes
-                let uiWindowScene = connectedScenes.first as? UIWindowScene
-                let currentController = uiWindowScene?.windows.first(where: \.isKeyWindow)?.rootViewController
-                let activityViewController = UIActivityViewController(activityItems: [fileUrl], applicationActivities: nil)
-                currentController?.present(activityViewController, animated: true, completion: {})
-            }
-        }
-    }
-    
-    func registerWithDigitalCredentialsAPI(entries: CredentialList, scope: any Kotlinx_coroutines_coreCoroutineScope) {
-        // TODO: Implement
-    }
-    
-    func getCurrentDCAPIData() -> KmmResult<Openid_data_classesDCAPIRequest> {
-        return KmmResult(failure: KotlinThrowable(message: "Using Swift platform adapter"))
-    }
-    
-    func prepareDCAPIIsoMdocCredentialResponse(
-        responseJson: KotlinByteArray,
-        sessionTranscript: KotlinByteArray,
-        encryptionParameters: Openid_data_classesEncryptionParameters
-    ) {
-        // TODO: Implement
-    }
-    
-    func prepareDCAPIOid4vpCredentialResponse(responseJson: String, success: Bool) {
-        // TODO: Implement
-    }
-    
-    func prepareDCAPIPreviewCredentialResponse(responseJson: KotlinByteArray, dcApiRequestPreview: Openid_data_classesPreviewDCAPIRequest) {
-        // TODO: Implement
-    }
-    
 }
 
--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -2,17 +2,26 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.ComposeUIViewController
+import at.asitplus.KmmResult
+import at.asitplus.dcapi.request.DCAPIRequest
+import at.asitplus.dcapi.request.PreviewDCAPIRequest
+import at.asitplus.iso.EncryptionParameters
 import at.asitplus.wallet.app.common.BuildContext
 import at.asitplus.wallet.app.common.KeystoreService
 import at.asitplus.wallet.app.common.PlatformAdapter
 import at.asitplus.wallet.app.common.WalletDependencyProvider
-import at.asitplus.wallet.app.common.WalletKeyMaterial
-import at.asitplus.wallet.app.common.WalletMain
+import at.asitplus.wallet.app.common.dcapi.data.export.CredentialList
 import data.storage.RealDataStoreService
 import data.storage.createDataStore
+import io.github.aakira.napier.Napier
+import kotlinx.cinterop.*
+import kotlinx.coroutines.CoroutineScope
 import org.multipaz.compose.prompt.PromptDialogs
 import org.multipaz.prompt.IosPromptModel
-import platform.UIKit.UIViewController
+import platform.Foundation.*
+import platform.UIKit.*
+import platform.darwin.dispatch_async
+import platform.darwin.dispatch_get_main_queue
 import ui.theme.darkScheme
 import ui.theme.lightScheme
 
@@ -28,10 +37,10 @@ actual fun getColorScheme(): ColorScheme {
 }
 
 fun MainViewController(
-    platformAdapter: PlatformAdapter,
     buildContext: BuildContext,
 ): UIViewController {
-    val dataStoreService = RealDataStoreService(createDataStore(), platformAdapter)
+    val iosPlatformAdapter = IosPlatformAdapter()
+    val dataStoreService = RealDataStoreService(createDataStore(), iosPlatformAdapter)
     val keystoreService = KeystoreService(dataStoreService)
     val promptModel = IosPromptModel()
 
@@ -41,10 +50,200 @@ fun MainViewController(
             WalletDependencyProvider(
                 keystoreService,
                 dataStoreService,
-                platformAdapter,
-                buildContext =  buildContext,
+                iosPlatformAdapter,
+                buildContext = buildContext,
                 promptModel = promptModel
             )
+        )
+    }
+}
+
+class IosPlatformAdapter(
+) : PlatformAdapter {
+    override fun openUrl(url: String) {
+        val url = NSURL(string = url)
+        if (UIApplication.sharedApplication.canOpenURL(url)) {
+            UIApplication.sharedApplication.openURL(url, mapOf<Any?, Any?>(), null)
+        }
+    }
+
+    @OptIn(ExperimentalForeignApi::class)
+    override fun writeToFile(text: String, fileName: String, folderName: String) {
+        val baseUrl = getBaseUrl() ?: return
+
+        val folderUrl = baseUrl.URLByAppendingPathComponent(folderName)
+        val folderPath = folderUrl?.path ?: return
+
+        val fileManager = NSFileManager.defaultManager
+        if (!fileManager.fileExistsAtPath(folderPath)) {
+            fileManager.createDirectoryAtPath(
+                path = folderPath,
+                withIntermediateDirectories = true,
+                attributes = null,
+                error = null
+            )
+        }
+
+        val fileUrl = folderUrl.URLByAppendingPathComponent(fileName)
+        val filePath = fileUrl?.path ?: return
+
+        val data = text.encodeToByteArray().toNSData()
+
+        if (fileManager.fileExistsAtPath(filePath)) {
+            val handle = NSFileHandle.fileHandleForWritingAtPath(filePath)
+            handle?.seekToEndOfFile()
+            handle?.writeData(data)
+            handle?.closeFile()
+        } else {
+            fileManager.createFileAtPath(
+                path = filePath,
+                contents = data,
+                attributes = null
+            )
+        }
+    }
+
+    @OptIn(ExperimentalForeignApi::class, BetaInteropApi::class)
+    override fun readFromFile(fileName: String, folderName: String): String? {
+        val baseUrl = getBaseUrl() ?: return null
+
+        val folderUrl = baseUrl.URLByAppendingPathComponent(folderName)
+        val folderPath = folderUrl?.path ?: return null
+
+        val fileManager = NSFileManager.defaultManager
+        if (!fileManager.fileExistsAtPath(folderPath)) {
+            fileManager.createDirectoryAtPath(
+                path = folderPath,
+                withIntermediateDirectories = true,
+                attributes = null,
+                error = null
+            )
+        }
+
+        val fileUrl = folderUrl.URLByAppendingPathComponent(fileName)
+        val filePath = fileUrl?.path ?: return null
+
+        memScoped {
+            val errorPtr = alloc<ObjCObjectVar<NSError?>>()
+            val content = NSString.stringWithContentsOfFile(
+                path = filePath,
+                encoding = NSUTF8StringEncoding,
+                error = errorPtr.ptr
+            )
+            errorPtr.value?.let {
+                Napier.e("Unable to read file: $fileName")
+            }
+            return content
+        }
+    }
+
+    @OptIn(ExperimentalForeignApi::class, BetaInteropApi::class)
+    override fun clearFile(fileName: String, folderName: String) {
+        val baseUrl = getBaseUrl() ?: return
+
+        val folderUrl = baseUrl.URLByAppendingPathComponent(folderName)
+        val folderPath = folderUrl?.path ?: return
+
+        val fileManager = NSFileManager.defaultManager
+
+        if (!fileManager.fileExistsAtPath(folderPath)) {
+            fileManager.createDirectoryAtPath(
+                path = folderPath,
+                withIntermediateDirectories = true,
+                attributes = null,
+                error = null
+            )
+        }
+
+        val fileUrl = folderUrl.URLByAppendingPathComponent(fileName)
+        val filePath = fileUrl?.path ?: return
+
+        memScoped {
+            val errorPtr = alloc<ObjCObjectVar<NSError?>>()
+            fileManager.removeItemAtPath(
+                path = filePath,
+                error = errorPtr.ptr
+            )
+            errorPtr.value?.let {
+                Napier.e("Unable to clear file: $fileName")
+            }
+        }
+    }
+
+    override fun shareLog() {
+        val baseUrl = getBaseUrl() ?: return
+
+        val folderUrl = baseUrl.URLByAppendingPathComponent("logs")
+        val fileUrl = folderUrl?.URLByAppendingPathComponent("log.txt") ?: return
+
+        dispatch_async(dispatch_get_main_queue()) {
+            val connectedScenes = UIApplication.sharedApplication.connectedScenes
+            val windowScene = (connectedScenes as NSSet)
+                .anyObject() as? UIWindowScene
+            val currentController = windowScene
+                ?.windows
+                ?.firstOrNull { (it as? UIWindow)?.isKeyWindow() == true }
+                ?.let { (it as UIWindow).rootViewController() }
+
+            val activityVC = UIActivityViewController(
+                activityItems = listOf(fileUrl),
+                applicationActivities = null
+            )
+
+            currentController?.presentViewController(
+                activityVC,
+                animated = true,
+                completion = null
+            )
+        }
+    }
+
+    override fun registerWithDigitalCredentialsAPI(
+        entries: CredentialList,
+        scope: CoroutineScope
+    ) {
+        //TODO("Not yet implemented")
+    }
+
+    override fun getCurrentDCAPIData(): KmmResult<DCAPIRequest> {
+        return KmmResult.failure(Throwable("Using Swift platform adapter"))
+    }
+
+    override fun prepareDCAPIPreviewCredentialResponse(
+        responseJson: ByteArray,
+        dcApiRequestPreview: PreviewDCAPIRequest
+    ) {
+        //TODO("Not yet implemented")
+    }
+
+    override fun prepareDCAPIIsoMdocCredentialResponse(
+        responseJson: ByteArray,
+        sessionTranscript: ByteArray,
+        encryptionParameters: EncryptionParameters
+    ) {
+        //TODO("Not yet implemented")
+    }
+
+    override fun prepareDCAPIOid4vpCredentialResponse(responseJson: String, success: Boolean) {
+        //TODO("Not yet implemented")
+    }
+
+    fun getBaseUrl(): NSURL? {
+        val urls = NSFileManager.defaultManager.URLsForDirectory(
+            directory = NSDocumentDirectory,
+            inDomains = NSUserDomainMask
+        )
+        return urls.first() as? NSURL
+    }
+
+}
+
+@OptIn(ExperimentalForeignApi::class)
+fun ByteArray.toNSData(): NSData = memScoped {
+    this@toNSData.usePinned { pinned ->
+        NSData.create(
+            bytes = pinned.addressOf(0),
+            length = this@toNSData.size.toULong()
         )
     }
 }
