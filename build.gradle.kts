eafbb6121c8c309b8e1e4753e62a5d0b0881d4b0
Michael <144923189+md4096@users.noreply.github.com>
2025-10-28 16:16:48 +0100

    Remove linker workaround

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
-import org.jetbrains.kotlin.konan.target.Family
-import java.io.ByteArrayOutputStream
-
 plugins {
     val kotlinVer =
         System.getenv("KOTLIN_VERSION_ENV")?.ifBlank { null } ?: libs.versions.kotlin.get()
@@ -20,47 +15,4 @@ plugins {
 
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.compose.compiler) version kotlinVer apply false
-}
-
-subprojects {
-    afterEvaluate {
-        /*help the linker (yes, this is absolutely bonkers!)*/
-        if (org.gradle.internal.os.OperatingSystem.current() == org.gradle.internal.os.OperatingSystem.MAC_OS) {
-            val devDir = System.getenv("DEVELOPER_DIR")?.ifEmpty { null }.let {
-                if (it == null) {
-                    val output = ByteArrayOutputStream()
-                    project.exec {
-                        commandLine("xcode-select", "-p")
-                        standardOutput = output
-                    }
-                    output.toString().trim()
-                } else it
-            }
-
-            logger.lifecycle("  DEV DIR points to $devDir")
-            val swiftLib = "$devDir/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/"
-
-            extensions.getByType<KotlinMultiplatformExtension>().targets.withType<KotlinNativeTarget>()
-                .configureEach {
-                    val sub = when (konanTarget.family) {
-                        Family.IOS ->
-                            if (konanTarget.name.contains("SIMULATOR", true)) "iphonesimulator" else "iphoneos"
-                        Family.TVOS ->
-                            if (konanTarget.name.contains("SIMULATOR", true)) "appletvsimulator" else "appletvos"
-                        Family.WATCHOS ->
-                            if (konanTarget.name.contains("SIMULATOR", true)) "watchsimulator" else "watchos"
-                        Family.OSX -> "macosx"
-                        else -> throw StopExecutionException("Konan target ${konanTarget.name} is not recognized")
-                    }
-
-                    logger.lifecycle("  KONAN target is ${konanTarget.name} which resolves to $sub")
-                    binaries.all {
-                        linkerOpts(
-                            "-L${swiftLib}$sub",
-                            "-L/usr/lib/swift"
-                        )
-                    }
-                }
-        }
-    }
 }
