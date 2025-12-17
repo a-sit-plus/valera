f978b6a4a4254b21e4e747fd45be37221106558f
GP <gerald.palfinger@a-sit.at>
2025-12-17 14:32:15 +0100

    Store app data in group for DC API

--- shared/src/iosMain/kotlin/data/storage/DataStore.ios.kt
+++ shared/src/iosMain/kotlin/data/storage/DataStore.ios.kt
@@ -3,23 +3,17 @@ package data.storage
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import kotlinx.cinterop.ExperimentalForeignApi
-import platform.Foundation.NSDocumentDirectory
 import platform.Foundation.NSFileManager
 import platform.Foundation.NSURL
-import platform.Foundation.NSUserDomainMask
-
 
 // Modified from https://github.com/android/kotlin-multiplatform-samples/tree/main
 @OptIn(ExperimentalForeignApi::class)
 fun createDataStore(): DataStore<Preferences> = getDataStore(
     producePath = {
-        val documentDirectory: NSURL? = NSFileManager.defaultManager.URLForDirectory(
-            directory = NSDocumentDirectory,
-            inDomain = NSUserDomainMask,
-            appropriateForURL = null,
-            create = false,
-            error = null,
-        )
-        requireNotNull(documentDirectory).path + "/$dataStoreFileName"
+        val appGroupIdentifier = "group.Compose.Wallet"
+        val containerURL: NSURL? = NSFileManager.defaultManager.containerURLForSecurityApplicationGroupIdentifier(appGroupIdentifier)
+        requireNotNull(containerURL) {
+            "Could not get container URL for app group $appGroupIdentifier. Make sure the app group is configured correctly."
+        }.path + "/$dataStoreFileName"
     }
 )
