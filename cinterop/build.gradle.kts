a784e3c1618dbf886ae6d381ede3fc98a42fbe7f
Gerald Palfinger <gerald.palfinger@a-sit.at>
2026-01-12 14:49:11 +0100

    Fix non-macOS build

--- cinterop/build.gradle.kts
+++ cinterop/build.gradle.kts
@@ -2,28 +2,34 @@ plugins {
     base
 }
 // Adapted from https://github.com/openwallet-foundation/multipaz
-listOf("iphoneos", "iphonesimulator").forEach { sdk ->
-    val taskName = "build${sdk.replaceFirstChar { it.titlecase() }}"
+val isMacHost = System.getProperty("os.name").lowercase().contains("mac")
 
-    tasks.register<Exec>(taskName) {
-        group = "build"
-        workingDir = projectDir
+if (isMacHost) {
+    listOf("iphoneos", "iphonesimulator").forEach { sdk ->
+        val taskName = "build${sdk.replaceFirstChar { it.titlecase() }}"
 
-        commandLine(
-            "xcodebuild",
-            "-project", "DigitalCredentials.xcodeproj",
-            "-scheme", "DigitalCredentials",
-            "-sdk", sdk,
-            "-configuration", "Release",
-            "SYMROOT=${projectDir}/build"
-        )
+        tasks.register<Exec>(taskName) {
+            group = "build"
+            workingDir = projectDir
 
-        inputs.files(
-            fileTree("$projectDir/DigitalCredentials.xcodeproj") { exclude("**/xcuserdata") },
-            fileTree("$projectDir/DigitalCredentials")
-        )
-        outputs.files(
-            fileTree("$projectDir/build/Release-${sdk}")
-        )
+            onlyIf { isMacHost }
+
+            commandLine(
+                "xcodebuild",
+                "-project", "DigitalCredentials.xcodeproj",
+                "-scheme", "DigitalCredentials",
+                "-sdk", sdk,
+                "-configuration", "Release",
+                "SYMROOT=${projectDir}/build"
+            )
+
+            inputs.files(
+                fileTree("$projectDir/DigitalCredentials.xcodeproj") { exclude("**/xcuserdata") },
+                fileTree("$projectDir/DigitalCredentials")
+            )
+            outputs.files(
+                fileTree("$projectDir/build/Release-${sdk}")
+            )
+        }
     }
 }
