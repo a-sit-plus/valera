665cd07ec591df6dc30ce883bcfdfde34897d57b
Gerald Palfinger <gerald.palfinger@a-sit.at>
2026-01-12 16:45:39 +0100

    Move duplicated code to extension method

--- shared/src/commonMain/kotlin/ui/viewmodels/authentication/NewDCAPIAuthenticationViewModel.kt
+++ shared/src/commonMain/kotlin/ui/viewmodels/authentication/NewDCAPIAuthenticationViewModel.kt
@@ -3,16 +3,11 @@ package ui.viewmodels.authentication
 import androidx.compose.ui.graphics.ImageBitmap
 import at.asitplus.catchingUnwrapped
 import at.asitplus.dcapi.request.DCAPIWalletRequest
-import at.asitplus.dif.Constraint
-import at.asitplus.dif.ConstraintField
 import at.asitplus.dif.DifInputDescriptor
-import at.asitplus.dif.FormatContainerJwt
-import at.asitplus.dif.FormatHolder
 import at.asitplus.dif.PresentationDefinition
 import at.asitplus.iso.DeviceRequest
-import at.asitplus.jsonpath.core.NormalizedJsonPath
-import at.asitplus.jsonpath.core.NormalizedJsonPathSegment
 import at.asitplus.wallet.app.common.WalletMain
+import at.asitplus.wallet.app.common.toDifInputDescriptorList
 import at.asitplus.wallet.lib.agent.SubjectCredentialStore
 import at.asitplus.wallet.lib.data.CredentialPresentation
 import at.asitplus.wallet.lib.data.CredentialPresentationRequest
@@ -43,25 +38,7 @@ class NewDCAPIAuthenticationViewModel(
     private var descriptors: List<DifInputDescriptor> = listOf()
 
     fun initWithDeviceRequest(parsedRequest: DeviceRequest) {
-        descriptors = parsedRequest.docRequests.map {
-            val itemsRequest = it.itemsRequest.value
-            DifInputDescriptor(
-                id = itemsRequest.docType,
-                format = FormatHolder(msoMdoc = FormatContainerJwt()),
-                constraints = Constraint(fields = itemsRequest.namespaces.flatMap { requestedNamespace ->
-                    requestedNamespace.value.entries.map { requestedAttribute ->
-                        ConstraintField(
-                            path = listOf(
-                                NormalizedJsonPath(
-                                    NormalizedJsonPathSegment.NameSegment(requestedNamespace.key),
-                                    NormalizedJsonPathSegment.NameSegment(requestedAttribute.key),
-                                ).toString()
-                            ), intentToRetain = requestedAttribute.value
-                        )
-                    }
-                }.toSet())
-            )
-        }
+        descriptors = parsedRequest.docRequests.toDifInputDescriptorList()
     }
 
     override val transactionData = null
