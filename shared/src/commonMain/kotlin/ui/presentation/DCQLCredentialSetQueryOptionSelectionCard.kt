074c72bf9517c0eeaa627c7c21a06554e7d9d410
acrusage <139230478+acrusage-iaik@users.noreply.github.com>
2026-02-16 20:17:01 +0100

    Change: Cleaner DCQL credential set query option selection UI

--- shared/src/commonMain/kotlin/ui/presentation/DCQLCredentialSetQueryOptionSelectionCard.kt
+++ shared/src/commonMain/kotlin/ui/presentation/DCQLCredentialSetQueryOptionSelectionCard.kt
@@ -1,34 +1,63 @@
 package ui.presentation
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.Card
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import at.asitplus.wallet.app.common.thirdParty.at.asitplus.wallet.lib.data.uiLabel
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import kotlin.random.Random
+import kotlin.time.Duration.Companion.seconds
 
 @Composable
-fun DCQLCredentialSetQueryOptionSelectionCard(
+fun ColumnScope.DCQLCredentialSetQueryOptionSelectionCard(
     isSatisfiable: Boolean,
     credentialQueryUiModels: List<DCQLCredentialQueryUiModel>,
     isSelected: Boolean,
-    onSelectCredentialQuery: () -> Unit,
+    onSelectCredentialSetQuery: () -> Unit,
 ) {
-    Card(
-        enabled = isSatisfiable,
-        onClick = onSelectCredentialQuery,
+    ElevatedCard(
+        onClick = onSelectCredentialSetQuery,
+        colors = when {
+            isSatisfiable -> CardDefaults.elevatedCardColors()
+
+            /**
+             * Let's still allow selecting unsatisfiable ones
+             */
+            else -> CardDefaults.elevatedCardColors(
+                containerColor = MaterialTheme.colorScheme.errorContainer,
+                contentColor = MaterialTheme.colorScheme.onErrorContainer,
+            )
+        },
+        modifier = Modifier.fillMaxWidth(),
     ) {
         Row(
             horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
         ) {
-            RadioButton(
-                selected = isSelected,
-                onClick = onSelectCredentialQuery,
-                enabled = isSatisfiable,
-            )
-            Column {
-                credentialQueryUiModels.forEach {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.padding(8.dp)
+            ) {
+                credentialQueryUiModels.forEachIndexed { index, it ->
                     CredentialSetQueryOptionSelectionCardCredentialQueryContent(
                         credentialRepresentationLocalized = it.credentialRepresentationLocalized,
                         credentialSchemeLocalized = it.credentialSchemeLocalized,
@@ -38,6 +67,11 @@ fun DCQLCredentialSetQueryOptionSelectionCard(
                     )
                 }
             }
+            RadioButton(
+                selected = isSelected,
+                onClick = onSelectCredentialSetQuery,
+                enabled = isSatisfiable,
+            )
         }
     }
 }
