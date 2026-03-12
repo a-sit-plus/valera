0b114068b303c4bb0297ab51a115f53f2e9adf70
Michael <144923189+md4096@users.noreply.github.com>
2026-03-12 14:40:46 +0100

    Improve error handling for attestation preload

--- shared/src/commonMain/kotlin/ui/navigation/WalletNavigation.kt
+++ shared/src/commonMain/kotlin/ui/navigation/WalletNavigation.kt
@@ -885,7 +885,10 @@ private fun WalletNavHost(
                 onClickLogo = onClickLogo,
                 onClickBack = navigateBack,
                 onClickSettings = navigateBack,
-                vm  = remember { AttestationSettingsViewModel(walletMain.attestationService) }
+                vm  = remember { AttestationSettingsViewModel(walletMain.attestationService) },
+                onError = {
+                    walletMain.errorService.emit(it)
+                }
             )
         }
     }
