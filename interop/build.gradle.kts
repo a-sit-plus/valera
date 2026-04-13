620b43daf14a17c99ff1acd84a8e04ef43609161
Gerald Palfinger <gerald.palfinger@a-sit.at>
2026-04-13 10:48:43 +0200

    Remove hardcoded Xcode paths

--- interop/build.gradle.kts
+++ interop/build.gradle.kts
@@ -4,6 +4,19 @@ plugins {
 
 kotlin {
     val isMacHost = System.getProperty("os.name").lowercase().contains("mac")
+    val developerDir = if (isMacHost) {
+        project.providers.environmentVariable("DEVELOPER_DIR")
+            .map { it.trim() }
+            .orElse(
+                project.providers.exec {
+                    commandLine("xcode-select", "-p")
+                }.standardOutput.asText.map { it.trim() }
+            )
+            .orNull
+            ?.ifBlank { null }
+    } else {
+        null
+    }
 
     val iosTargets = listOf(iosArm64(), iosSimulatorArm64())
 
@@ -13,12 +26,26 @@ kotlin {
             "iosSimulatorArm64" -> "iphonesimulator"
             else -> error("Unsupported target ${target.name}")
         }
+        val platformVersion = when (platform) {
+            "iphoneos" -> "ios 26.0 26.0"
+            "iphonesimulator" -> "ios-simulator 26.0 26.0"
+            else -> error("Unsupported platform $platform")
+        }
+        val swiftLibPath = developerDir
+            ?.let { "$it/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/$platform/" }
 
         target.compilations.getByName("main") {
             cinterops {
                 val DigitalCredentials by creating {
                     definitionFile.set(file("${rootDir}/cinterop/DigitalCredentials-${platform}.def"))
                     includeDirs.headerFilterOnly("${rootDir}/cinterop/build/Release-${platform}/include")
+                    if (swiftLibPath != null) {
+                        linkerOpts(
+                            "-platform_version",
+                            *platformVersion.split(" ").toTypedArray(),
+                            "-L$swiftLibPath"
+                        )
+                    }
 
                     // Only make cinterop processing depend on Xcode builds when on macOS
                     if (isMacHost) {
@@ -33,11 +60,16 @@ kotlin {
 
         if (isMacHost) {
             target.binaries.all {
-                linkerOpts(
-                    "-L/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/${platform}/",
-                    "-L${rootDir}/cinterop/build/Release-${platform}",
-                    "-lDigitalCredentials"
-                )
+                val opts = buildList {
+                    if (swiftLibPath != null) {
+                        add("-platform_version")
+                        addAll(platformVersion.split(" "))
+                        add("-L$swiftLibPath")
+                    }
+                    add("-L${rootDir}/cinterop/build/Release-${platform}")
+                    add("-lDigitalCredentials")
+                }
+                linkerOpts(*opts.toTypedArray())
             }
         }
     }
