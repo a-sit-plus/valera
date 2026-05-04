4bd9c21138e583f81d6bd312cf3c013859f5605e
Gerald Palfinger <gerald.palfinger@a-sit.at>
2026-05-04 18:22:50 +0200

    Use flow-local bindings for Koin objects to avoid state issues between normal and sharing app instances

--- androidApp/src/androidMain/kotlin/at/asitplus/wallet/app/android/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/at/asitplus/wallet/app/android/MainActivity.kt
@@ -2,40 +2,52 @@ package at.asitplus.wallet.app.android
 
 import MainView
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import at.asitplus.wallet.app.common.BuildContext
-import at.asitplus.wallet.app.common.BuildType
+import at.asitplus.wallet.app.common.IntentState
+import at.asitplus.wallet.app.common.SessionService
 import io.github.aakira.napier.Napier
+import org.koin.core.context.GlobalContext
 import org.multipaz.prompt.AndroidPromptModel
 import org.multipaz.prompt.PromptModel
 
 
 class MainActivity : AbstractWalletActivity() {
-    private val intentState = AndroidIntentStateHolder.intentState
+    private val intentState = IntentState()
+    private val buildContext by lazy { createBuildContext() }
+    private val promptModel: PromptModel by lazy {
+        AndroidPromptModel.Builder().apply { addCommonDialogs() }.build()
+    }
+    private lateinit var sessionService: SessionService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         intentState.finishApp = { finish() }
 
-        val promptModel: PromptModel by lazy {
-            AndroidPromptModel.Builder().apply { addCommonDialogs() }.build()
+        if (!::sessionService.isInitialized) {
+            sessionService = SessionService().apply {
+                initialize {
+                    createWalletSessionScope(
+                        koin = GlobalContext.get(),
+                        sessionName = "main",
+                        activity = this@MainActivity,
+                        intentState = intentState,
+                        sessionService = this,
+                        buildContext = buildContext,
+                        promptModel = promptModel
+                    )
+                }
+            }
         }
 
         setContent {
             MainView(
-                buildContext = BuildContext(
-                    buildType = BuildType.valueOf(BuildConfig.BUILD_TYPE.uppercase()),
-                    packageName = BuildConfig.APPLICATION_ID,
-                    versionCode = BuildConfig.VERSION_CODE,
-                    versionName = BuildConfig.VERSION_NAME,
-                    osVersion = "Android ${Build.VERSION.RELEASE}"
-                ),
-                promptModel,
-                intentState = intentState
+                buildContext = buildContext,
+                promptModel = promptModel,
+                intentState = intentState,
+                sessionService = sessionService
             )
         }
     }
@@ -48,5 +60,12 @@ class MainActivity : AbstractWalletActivity() {
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         populateLink(intent)
+    }
+
+    override fun onDestroy() {
+        if (::sessionService.isInitialized && isFinishing) {
+            sessionService.close()
+        }
+        super.onDestroy()
     }
 }
--- shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/domain/di/DomainModule.kt
+++ shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/domain/di/DomainModule.kt
@@ -1,13 +1,12 @@
 package at.asitplus.wallet.app.common.domain.di
 
-import at.asitplus.wallet.app.common.WalletDependencyProvider
 import at.asitplus.wallet.app.common.domain.communications.di.communicationsModule
 import at.asitplus.wallet.app.common.domain.platform.di.platformModule
 import at.asitplus.wallet.app.common.domain.vck.di.vckModule
 import org.koin.dsl.module
 
-fun domainModule(walletDependencyProvider: WalletDependencyProvider) = module {
-    includes(platformModule(walletDependencyProvider))
+fun domainModule() = module {
+    includes(platformModule())
     includes(communicationsModule())
     includes(vckModule())
 }
