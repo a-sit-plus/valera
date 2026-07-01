faf48d234344ace21f7f0226d189c68e48405949
Christian Kollmann <christian.kollmann@a-sit.at>
2026-07-01 14:14:24 +0200

    Remove unused code

--- shared/src/commonMain/kotlin/data/PersonalDataCategory.kt
+++ shared/src/commonMain/kotlin/data/PersonalDataCategory.kt
@@ -1,8 +1,6 @@
 package data
 
 import at.asitplus.valera.resources.Res
-import at.asitplus.valera.resources.section_heading_admission_data
-import at.asitplus.valera.resources.section_heading_admission_data_icon_text
 import at.asitplus.valera.resources.section_heading_age_data
 import at.asitplus.valera.resources.section_heading_age_data_icon_text
 import at.asitplus.valera.resources.section_heading_appearance_data
@@ -11,8 +9,6 @@ import at.asitplus.valera.resources.section_heading_biometric_data
 import at.asitplus.valera.resources.section_heading_biometric_data_icon_text
 import at.asitplus.valera.resources.section_heading_birth_data
 import at.asitplus.valera.resources.section_heading_birth_data_icon_text
-import at.asitplus.valera.resources.section_heading_company_data
-import at.asitplus.valera.resources.section_heading_company_data_icon_text
 import at.asitplus.valera.resources.section_heading_driving_permission_data
 import at.asitplus.valera.resources.section_heading_driving_permission_data_icon_text
 import at.asitplus.valera.resources.section_heading_identity_data
@@ -21,8 +17,6 @@ import at.asitplus.valera.resources.section_heading_metadata
 import at.asitplus.valera.resources.section_heading_metadata_icon_text
 import at.asitplus.valera.resources.section_heading_other_data_category_icon_text
 import at.asitplus.valera.resources.section_heading_other_data_category_title
-import at.asitplus.valera.resources.section_heading_representation_data
-import at.asitplus.valera.resources.section_heading_representation_data_icon_text
 import at.asitplus.valera.resources.section_heading_residence_data
 import at.asitplus.valera.resources.section_heading_residence_data_icon_text
 import org.jetbrains.compose.resources.StringResource
@@ -47,14 +41,6 @@ enum class PersonalDataCategory(
         iconText = Res.string.section_heading_biometric_data_icon_text,
         categoryTitle = Res.string.section_heading_biometric_data,
     ),
-    RepresentationData(
-        iconText = Res.string.section_heading_representation_data_icon_text,
-        categoryTitle = Res.string.section_heading_representation_data,
-    ),
-    CompanyData(
-        iconText = Res.string.section_heading_company_data_icon_text,
-        categoryTitle = Res.string.section_heading_company_data,
-    ),
     AgeData(
         iconText = Res.string.section_heading_age_data_icon_text,
         categoryTitle = Res.string.section_heading_age_data,
@@ -66,10 +52,6 @@ enum class PersonalDataCategory(
     DrivingPermissions(
         iconText = Res.string.section_heading_driving_permission_data_icon_text,
         categoryTitle = Res.string.section_heading_driving_permission_data
-    ),
-    AdmissionData(
-        iconText = Res.string.section_heading_admission_data_icon_text,
-        categoryTitle = Res.string.section_heading_admission_data
     ),
     Metadata(
         iconText = Res.string.section_heading_metadata_icon_text,
