5894eb964e1c75c096e9280c20493124f8cffae1
Srdjan Stjepanovic <srdjan.stjepanovic@a-sit.at>
2026-07-28 11:38:46 +0200

    rebasing onto development

--- shared/src/commonMain/kotlin/ui/composables/IssuerTrustDetailsCard.kt
+++ shared/src/commonMain/kotlin/ui/composables/IssuerTrustDetailsCard.kt
@@ -31,9 +31,11 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import at.asitplus.signum.indispensable.Digest
+import at.asitplus.signum.indispensable.X509SignatureAlgorithm
 import at.asitplus.signum.indispensable.asn1.encoding.decodeToString
 import at.asitplus.signum.indispensable.pki.AttributeTypeAndValue
 import at.asitplus.signum.indispensable.pki.X509Certificate
+import at.asitplus.signum.indispensable.requireSupported
 import at.asitplus.signum.supreme.hash.digest
 import at.asitplus.valera.resources.Res
 import at.asitplus.valera.resources.button_label_hide_technical_details
@@ -49,6 +51,7 @@ import at.asitplus.valera.resources.trust_status_evaluating
 import at.asitplus.valera.resources.trust_status_trusted
 import at.asitplus.valera.resources.trust_status_unknown
 import at.asitplus.valera.resources.trust_status_untrusted
+import at.asitplus.wallet.app.common.digest
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
@@ -60,20 +63,17 @@ fun IssuerTrustDetailsCard(
     var showTechnicalDetails by remember { mutableStateOf(false) }
     val contentPadding = Modifier.padding(bottom = 16.dp, end = 16.dp, start = 16.dp)
 
-
     val rdnAttributes = certificate.tbsCertificate.subjectName.flatMap { it.attrsAndValues }
-
     val issuerName = rdnAttributes.firstOrNull { it is AttributeTypeAndValue.Organization }?.value?.asPrimitive()?.decodeToString()
         ?: rdnAttributes.firstOrNull { it is AttributeTypeAndValue.CommonName }?.value?.asPrimitive()?.decodeToString()
         ?: "Unknown Issuer"
 
-
     val validFrom = certificate.tbsCertificate.validFrom.instant.toString()
-
     val validUntil = certificate.tbsCertificate.validUntil.instant.toString()
-
-    val digest: String = Digest.SHA256.digest(certificate.encodeToDer()).contentToString()
-
+    val digest = certificate.signatureAlgorithm.let {
+        it.requireSupported()
+        it.digest.digest(certificate.encodeToDer()).contentToString()
+    }
     val displayTrust = when (trustState) {
         TrustState.TRUSTED -> stringResource(Res.string.trust_status_trusted)
         TrustState.UNTRUSTED -> stringResource(Res.string.trust_status_untrusted)
