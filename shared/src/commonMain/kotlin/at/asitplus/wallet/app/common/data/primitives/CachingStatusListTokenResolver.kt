476e369be9c7863e50e5c5cdff52ac213d7b938f
Michael <144923189+md4096@users.noreply.github.com>
2026-09-02 12:25:25 +0200

    Remove redundant StatusListTokenResolver (#552)

--- shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/data/primitives/CachingStatusListTokenResolver.kt
+++ shared/src/commonMain/kotlin/at/asitplus/wallet/app/common/data/primitives/CachingStatusListTokenResolver.kt
@@ -1,6 +1,6 @@
 package at.asitplus.wallet.app.common.data.primitives
 
-import at.asitplus.wallet.app.common.domain.vck.tokenStatusList.StatusListTokenResolver
+import at.asitplus.wallet.lib.agent.validation.StatusListTokenResolver
 import at.asitplus.wallet.lib.data.StatusListToken
 import at.asitplus.wallet.lib.data.rfc3986.UniformResourceIdentifier
 
@@ -11,8 +11,8 @@ data class CachingStatusListTokenResolver(
     val store: SimpleStore<UniformResourceIdentifier, StatusListToken>,
     val statusListTokenResolver: StatusListTokenResolver,
 ) : StatusListTokenResolver {
-    override suspend fun invoke(uniformResourceIdentifier: UniformResourceIdentifier) =
-        store.getOrPut(uniformResourceIdentifier) {
-            statusListTokenResolver(uniformResourceIdentifier)
+    override suspend fun invoke(statusListUrl: UniformResourceIdentifier) =
+        store.getOrPut(statusListUrl) {
+            statusListTokenResolver(statusListUrl)
         }
 }
