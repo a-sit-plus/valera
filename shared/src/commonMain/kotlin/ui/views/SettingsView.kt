c24430aaf81fde3e3839892c256f18e359df215e
Srdjan Stjepanovic <srdjan.stjepanovic@a-sit.at>
2026-09-15 10:27:22 +0200

    align with the vck updates

--- shared/src/commonMain/kotlin/ui/views/SettingsView.kt
+++ shared/src/commonMain/kotlin/ui/views/SettingsView.kt
@@ -42,7 +42,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
@@ -73,7 +72,7 @@ import at.asitplus.valera.resources.text_label_trust_list_urls
 import at.asitplus.valera.resources.text_supporting_openid4vp_allowed_origin_schemes
 import at.asitplus.valera.resources.warning
 import at.asitplus.wallet.app.common.BuildType
-import at.asitplus.wallet.lib.etsi.LoTEServiceType
+import at.asitplus.wallet.lib.etsi.LoteProfile
 import org.jetbrains.compose.resources.getString
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
@@ -81,7 +80,6 @@ import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.scope.Scope
 import ui.composables.CircularProgressIndicatorOverlay
 import ui.composables.DelayedComposable
-import ui.composables.LabeledText
 import ui.composables.Logo
 import ui.composables.ScreenHeading
 import ui.composables.buttons.NavigateUpButton
@@ -284,7 +282,7 @@ fun SettingsView(
                                 style = MaterialTheme.typography.titleSmall,
                                 modifier = Modifier.padding(start = 16.dp, bottom = 4.dp)
                             )
-                            LoTEServiceType.defaultUrls.forEach { url ->
+                            LoteProfile.defaultUrls.forEach { url ->
                                 Text(
                                     text = url,
                                     style = MaterialTheme.typography.bodyMedium,
